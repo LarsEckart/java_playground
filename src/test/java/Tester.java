2a4d40956d36805ed4c0f1a91d40c32f68dbd448
Lars Eckart <lars.eckart+github@gmail.com>
2016-10-24 20:32:41 +0300

    tried out example code from http://www.martinfowler.com/bliki/JunitNewInstance.html
    adds lint warnings to javacompile
    
    many more code where i tried out things regarding tests

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -1,6 +1,11 @@
 
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +14,9 @@ import static org.junit.Assert.assertEquals;
 public class Tester {
 
     private List<String> list = new ArrayList<>();
+
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
 
     @Test
     public void testFirst() {
@@ -19,5 +27,23 @@ public class Tester {
     @Test
     public void testSecond() {
         assertEquals(0, list.size());
+    }
+
+    @Test
+    public void countsAssets() throws IOException {
+        File icon = tempFolder.newFile("icon.png");
+        File assets = tempFolder.newFolder("assets");
+        createAssets(assets, 3);
+
+
+        //DigitalAssetManager dam = new DigitalAssetManager(icon, assets);
+        //assertEquals(3, dam.getAssetCount());
+    }
+
+    private void createAssets(File assets, int numberOfAssets) throws IOException {
+        for (int index = 0; index < numberOfAssets; index++) {
+            File asset = new File(assets, String.format("asset-%d.mpg", index));
+            Assert.assertTrue("Asset couldn't be created.", asset.createNewFile());
+        }
     }
 }
