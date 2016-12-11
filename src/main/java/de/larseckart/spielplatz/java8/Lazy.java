559254b0b62c0253fec8d62d9c322acc4386a5cb
Lars Eckart <lars.eckart+github@gmail.com>
2016-12-11 18:32:14 +0100

    add Lazy class from http://minborgsjavapot.blogspot.de/2016/01/be-lazy-with-java-8.html

---
+++ src/main/java/de/larseckart/spielplatz/java8/Lazy.java
@@ -0,0 +1,39 @@
+package de.larseckart.spielplatz.java8;
+
+import java.util.function.Supplier;
+
+import static java.util.Objects.requireNonNull;
+
+public final class Lazy<T> {
+
+    private volatile T value;
+
+    public T getOrCompute(Supplier<T> supplier) {
+        final T result = value;  // Read volatile just once...
+        return result == null ? maybeCompute(supplier) : result;
+    }
+
+    private synchronized T maybeCompute(Supplier<T> supplier) {
+        if (value == null) {
+            value = requireNonNull(supplier.get());
+        }
+        return value;
+    }
+
+    class Point {
+
+        private final int x, y;
+        private final Lazy<String> lazyToString;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+            lazyToString = new Lazy<>();
+        }
+
+        @Override
+        public String toString() {
+            return lazyToString.getOrCompute(() -> "(" + x + ", " + y + ")");
+        }
+    }
+}
