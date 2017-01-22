e3f34972aeaf7b92abc852fb7e5a88a141ae4a64
Lars Eckart <lars.eckart+github@gmail.com>
2017-01-22 21:40:25 +0200

    bump dependencies

--- src/main/java/de/larseckart/spielplatz/Main.java
+++ src/main/java/de/larseckart/spielplatz/Main.java
@@ -1,8 +1,16 @@
 package de.larseckart.spielplatz;
 
+import java.util.LinkedList;
+import java.util.Spliterator;
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        final Integer[] numbers = IntStream.rangeClosed(1, 10_000).boxed().toArray(Integer[]::new);
+
+        final Spliterator<String> spliterator = new LinkedList<String>().spliterator();
+        System.out.println(spliterator.hasCharacteristics(Spliterator.CONCURRENT));
     }
 }
