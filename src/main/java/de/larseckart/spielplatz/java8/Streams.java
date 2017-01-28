f7002948514cb6abd2d2ab393bb19e383f4b1a19
Lars Eckart <lars.eckart+github@gmail.com>
2017-01-28 19:29:29 +0200

    remove fortumo file header

--- src/main/java/de/larseckart/spielplatz/java8/Streams.java
+++ src/main/java/de/larseckart/spielplatz/java8/Streams.java
@@ -1,31 +1,3 @@
-/*
- * File: de.larseckart.spielplatz.java8.Streams
- * Created: 22:57 22/11/2015
- * Author: Lars Eckart <lars.eckart@fortumo.com>
- *
- * Copyright (C) Fortumo OÜ, 2015
- * All rights reserved. Proprietary and confidential.
- * Unauthorized copying of this file, via any medium is strictly prohibited.
- *
- * All rights reserved. Proprietary and confidential.
- * Unauthorized copying of this file, via any medium is strictly prohibited.
- * Distribution, modification, reproduction, merging, publishing, sub-licensing,
- * sale and/or any other use (in binary form or otherwise) of this software is
- * not permitted, unless Fortumo OÜ has explicitly and in writing permitted such
- * use of the software.
- *
- * THIS SOFTWARE IS PROVIDED BY COPYRIGHT HOLDER ''AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL Fortumo OÜ BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package de.larseckart.spielplatz.java8;
 
 import java.util.Arrays;
