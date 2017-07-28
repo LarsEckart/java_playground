da7aee5b84c69395e1ea1853c4196c4090f6f16e
Lars Eckart <lars.eckart@gmail.com>
2017-07-28 23:16:43 +0300

    figuring out how clever spring is when it comes to dependency order ;)

--- spring/src/main/java/ee/lars/spring/soundsystem/CDPlayerConfig.java
+++ spring/src/main/java/ee/lars/spring/soundsystem/CDPlayerConfig.java
@@ -12,15 +12,16 @@ public class CDPlayerConfig {
         return new SgtPeppers();
     }
 
+    /*
     @Bean
     public MediaPlayer cdPlayer() {
         return new SpringCDPlayer(sgtPeppers());
     }
+    */
 
-    /*
     @Bean
     public MediaPlayer anotherCdPlayer(CompactDisc cd) {
         return new SpringCDPlayer(cd);
     }
-    */
+
 }
