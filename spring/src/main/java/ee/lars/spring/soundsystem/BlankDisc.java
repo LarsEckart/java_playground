389555da56e0c66234c691621f24feef3a8fbfe8
Lars Eckart <lars.eckart@gmail.com>
2017-07-28 22:52:51 +0300

    Manually wiring up beans instead of using ComponentScan

---
+++ spring/src/main/java/ee/lars/spring/soundsystem/BlankDisc.java
@@ -0,0 +1,17 @@
+package ee.lars.spring.soundsystem;
+
+public class BlankDisc implements CompactDisc {
+
+    private final String title;
+    private final String artist;
+
+    public BlankDisc(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Playing " + this.title + " by " + this.artist);
+    }
+}
--- spring/src/main/java/ee/lars/spring/soundsystem/CDPlayerConfig.java
+++ spring/src/main/java/ee/lars/spring/soundsystem/CDPlayerConfig.java
@@ -1,10 +1,26 @@
 package ee.lars.spring.soundsystem;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
 public class CDPlayerConfig {
 
+    @Bean
+    public CompactDisc sgtPeppers() {
+        return new SgtPeppers();
+    }
+
+    @Bean
+    public MediaPlayer cdPlayer() {
+        return new SpringCDPlayer(sgtPeppers());
+    }
+
+    /*
+    @Bean
+    public MediaPlayer anotherCdPlayer(CompactDisc cd) {
+        return new SpringCDPlayer(cd);
+    }
+    */
 }
