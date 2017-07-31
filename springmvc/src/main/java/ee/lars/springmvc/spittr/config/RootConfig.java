99ecfb540bfa8ee10aab1b60c8f9cff6a1bf9e88
Lars Eckart <lars.eckart@gmail.com>
2017-07-31 22:59:17 +0300

    spring and jdbc, let the fun begin

--- springmvc/src/main/java/ee/lars/springmvc/spittr/config/RootConfig.java
+++ springmvc/src/main/java/ee/lars/springmvc/spittr/config/RootConfig.java
@@ -3,9 +3,11 @@ package ee.lars.springmvc.spittr.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@Import(DataConfig.class)
 @ComponentScan(basePackages = {"ee.lars.springmvc.spittr"},
         excludeFilters = {
                 @ComponentScan.Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)})
