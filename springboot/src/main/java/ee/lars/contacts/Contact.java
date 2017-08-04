431be4ba080a30caa6a15d7117d655c06bb64915
Lars Eckart <lars.eckart@gmail.com>
2017-08-04 23:35:34 +0300

    spring boot, web layer of the contacts app

--- springboot/src/main/java/ee/lars/contacts/Contact.java
+++ springboot/src/main/java/ee/lars/contacts/Contact.java
@@ -2,4 +2,44 @@ package ee.lars.contacts;
 
 public class Contact {
 
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String emailAddress;
+
+    public Contact(String firstName, String lastName, String phoneNumber, String emailAddress) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+    }
+
+    public Contact(Long id, String firstName, String lastName, String phoneNumber, String emailAddress) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return this.emailAddress;
+    }
 }
