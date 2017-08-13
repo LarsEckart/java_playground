b4b2b29e162dba99d9b1cf1e12770469d91437c6
Lars Eckart <lars.eckart@gmail.com>
2017-08-13 16:24:58 +0300

    Working effectively with legacy code - sprout method
    describe required change and add code directly to the existing code

--- any/src/main/java/ee/lars/books/TransactionGate.java
+++ any/src/main/java/ee/lars/books/TransactionGate.java
@@ -2,11 +2,14 @@ package ee.lars.books;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  * Working effectively with legacy code.
  * Sprout method.
+ * We need to add code to verify that none of the new entries are already in transactionBundle
+ * before we post their dates and add them.
  */
 public class TransactionGate {
 
@@ -14,13 +17,20 @@ public class TransactionGate {
 
     public void postEntries(List<Entry> entries) {
 
+        List<Entry> entriesToAdd = new LinkedList<>();
+
         for (Iterator<Entry> it = entries.iterator(); it.hasNext(); ) {
             Entry entry = it.next();
-            entry.postData();
+            if (!this.transactionBundle.getListManager().contains(entry)) {
+                entry.postData();
+                entriesToAdd.add(entry);
+            }
         }
 
-        this.transactionBundle.getListManager().addAll(entries);
+        this.transactionBundle.getListManager().addAll(entriesToAdd);
     }
+
+
 
     private class Entry {
 
