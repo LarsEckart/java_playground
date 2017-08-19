6c027fb45b6c6842299f7ecda69d50c3654513a4
Lars Eckart <lars.eckart@gmail.com>
2017-08-19 18:05:07 +0300

    Cracking the coding interview, chapter 1, string compression

--- cracking/src/test/java/ee/lars/cracking/chap1/StringCompressionTest.java
+++ cracking/src/test/java/ee/lars/cracking/chap1/StringCompressionTest.java
@@ -7,13 +7,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringCompressionTest {
 
     @Test
-    public void returns_original_string_for_string_with_length_1() throws Exception {
-        // given
+    public void returns_original_string_when_compressed_string_is_longer_than_original() throws Exception {
+        assertThat(StringCompression.compress("a")).isEqualTo("a");
+        assertThat(StringCompression.compress("aa")).isEqualTo("aa");
+        assertThat(StringCompression.compress("ab")).isEqualTo("ab");
+        assertThat(StringCompression.compress("abc")).isEqualTo("abc");
+        assertThat(StringCompression.compress("aabbcc")).isEqualTo("aabbcc");
+        assertThat(StringCompression.compress("aaabc")).isEqualTo("aaabc");
+        assertThat(StringCompression.compress("aaabbc")).isEqualTo("aaabbc");
+    }
 
-        // when
-        String compressed = StringCompression.compress("a");
-
-        // then
-        assertThat(compressed).isEqualTo("a");
+    @Test
+    public void returns_compressed_string_when_compressed_string_is_shorter_than_original() throws Exception {
+        assertThat(StringCompression.compress("aaa")).isEqualTo("a3");
+        assertThat(StringCompression.compress("aaabb")).isEqualTo("a3b2");
+        assertThat(StringCompression.compress("aaaaabbc")).isEqualTo("a5b2c1");
+        assertThat(StringCompression.compress("aabcccccaaa")).isEqualTo("a2b1c5a3");
     }
 }
