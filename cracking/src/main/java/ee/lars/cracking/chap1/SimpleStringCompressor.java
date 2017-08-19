72c2432c423c579f789a2fc82bb7e653adb4a0b1
Lars Eckart <lars.eckart@gmail.com>
2017-08-19 18:25:42 +0300

    Cracking the coding interview, chapter 1, string compression

---
+++ cracking/src/main/java/ee/lars/cracking/chap1/SimpleStringCompressor.java
@@ -0,0 +1,47 @@
+package ee.lars.cracking.chap1;
+
+public class SimpleStringCompressor {
+
+    private static final int ONE = 1;
+
+    public static String compress(String text) {
+        if (text.length() <= 1) {
+            return text;
+        }
+
+        StringBuilder strb = new StringBuilder();
+        final char[] chars = text.toCharArray();
+        int amount = ONE;
+
+        for (int i = 0; i < chars.length; i++) {
+            if (isLastCharacter(chars, i)) {
+                compress(strb, chars[i], amount);
+                amount = ONE;
+            } else {
+                if (currentCharacterIsFollowedBySameCharacter(chars, i)) {
+                    amount++;
+                } else {
+                    compress(strb, chars[i], amount);
+                    amount = ONE;
+                }
+            }
+        }
+
+        String compressed = strb.toString();
+
+        return (compressed.length() < text.length()) ? compressed : text;
+    }
+
+    private static boolean isLastCharacter(char[] chars, int i) {
+        return i == chars.length - 1;
+    }
+
+    private static boolean currentCharacterIsFollowedBySameCharacter(char[] chars, int i) {
+        return chars[i] == chars[i + 1];
+    }
+
+    private static void compress(StringBuilder strb, char currentChar, int amount) {
+        strb.append(currentChar);
+        strb.append(amount);
+    }
+}
---
+++ cracking/src/test/java/ee/lars/cracking/chap1/SimpleStringCompressorTest.java
@@ -0,0 +1,27 @@
+package ee.lars.cracking.chap1;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SimpleStringCompressorTest {
+
+    @Test
+    public void returns_original_string_when_compressed_string_is_longer_than_original() throws Exception {
+        assertThat(SimpleStringCompressor.compress("a")).isEqualTo("a");
+        assertThat(SimpleStringCompressor.compress("aa")).isEqualTo("aa");
+        assertThat(SimpleStringCompressor.compress("ab")).isEqualTo("ab");
+        assertThat(SimpleStringCompressor.compress("abc")).isEqualTo("abc");
+        assertThat(SimpleStringCompressor.compress("aabbcc")).isEqualTo("aabbcc");
+        assertThat(SimpleStringCompressor.compress("aaabc")).isEqualTo("aaabc");
+        assertThat(SimpleStringCompressor.compress("aaabbc")).isEqualTo("aaabbc");
+    }
+
+    @Test
+    public void returns_compressed_string_when_compressed_string_is_shorter_than_original() throws Exception {
+        assertThat(SimpleStringCompressor.compress("aaa")).isEqualTo("a3");
+        assertThat(SimpleStringCompressor.compress("aaabb")).isEqualTo("a3b2");
+        assertThat(SimpleStringCompressor.compress("aaaaabbc")).isEqualTo("a5b2c1");
+        assertThat(SimpleStringCompressor.compress("aabcccccaaa")).isEqualTo("a2b1c5a3");
+    }
+}
