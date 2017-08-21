16a718c0635a93e59d1478717646e438cc77ead1
Lars Eckart <lars.eckart@gmail.com>
2017-08-21 21:13:43 +0300

    Cracking the coding interview, chapter 1, String rotation
    Assume you have a method isSubstring which checks if one word is a
    substring of another. Given two strings, S1 and S2, write code to check
    if S2 is a rotation of S1 using only one call to isSubstring
    (e.g.,"waterbottle" is a rotation of"erbottlewat").

---
+++ cracking/src/test/java/ee/lars/cracking/chap1/StringRotationTest.java
@@ -0,0 +1,40 @@
+package ee.lars.cracking.chap1;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringRotationTest {
+
+    @Before
+    public void initialize() throws Exception {
+
+    }
+
+    @Test
+    public void should_return_true_when_word_is_rotation() throws Exception {
+        // given
+        String original = "waterbottle";
+        String rotated = "erbottlewat";
+
+        // when
+        final boolean result = StringRotation.isRotation(original, rotated);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void should_return_false_when_word_is_not_rotation() throws Exception {
+        // given
+        String original = "helloWorld";
+        String notRotated = "hollywood";
+
+        // when
+        final boolean result = StringRotation.isRotation(original, notRotated);
+
+        // then
+        assertThat(result).isFalse();
+    }
+}
