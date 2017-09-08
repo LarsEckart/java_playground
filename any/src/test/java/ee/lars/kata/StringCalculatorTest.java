149b599b5811f42d9fdc875ed18a803af8f65c98
Lars Eckart <lars.eckart@gmail.com>
2017-09-08 22:47:44 +0300

    improve readability of tests

--- any/src/test/java/ee/lars/kata/StringCalculatorTest.java
+++ any/src/test/java/ee/lars/kata/StringCalculatorTest.java
@@ -18,32 +18,38 @@ public class StringCalculatorTest {
 
     @Test
     public void returns_zero_for_empty_string() throws Exception {
-        assertThat(this.stringCalculator.add("")).isEqualTo(0);
+        final int sum = this.stringCalculator.add("");
+        assertThat(sum).isEqualTo(0);
     }
 
     @Test
     public void returns_the_number_if_only_one_number() throws Exception {
-        assertThat(this.stringCalculator.add("1")).isEqualTo(1);
+        final int sum = this.stringCalculator.add("1");
+        assertThat(sum).isEqualTo(1);
     }
 
     @Test
     public void returns_sum_of_two_numbers_separated_by_comma() throws Exception {
-        assertThat(this.stringCalculator.add("1,1")).isEqualTo(2);
+        final int sum = this.stringCalculator.add("1,1");
+        assertThat(sum).isEqualTo(2);
     }
 
     @Test
     public void returns_sum_of_any_amount_of_numbers_separated_by_comma() throws Exception {
-        assertThat(this.stringCalculator.add("1,2,3")).isEqualTo(6);
+        final int sum = this.stringCalculator.add("1,2,3");
+        assertThat(sum).isEqualTo(6);
     }
 
     @Test
     public void returns_sum_if_numbers_String_contains_newline_instead_of_comma() throws Exception {
-        assertThat(this.stringCalculator.add("1\n2,3")).isEqualTo(6);
+        final int sum = this.stringCalculator.add("1\n2,3");
+        assertThat(sum).isEqualTo(6);
     }
 
     @Test
     public void returns_sum_if_numbers_string_has_delimiter_provided_in_first_line() throws Exception {
-        assertThat(this.stringCalculator.add("//;\n1;2")).isEqualTo(3);
+        final int sum = this.stringCalculator.add("//;\n1;2");
+        assertThat(sum).isEqualTo(3);
     }
 
     @Test
@@ -61,17 +67,20 @@ public class StringCalculatorTest {
 
     @Test
     public void ignores_numbers_larger_than_thousand() throws Exception {
-        assertThat(this.stringCalculator.add("2,1001")).isEqualTo(2);
+        final int sum = this.stringCalculator.add("2,1001");
+        assertThat(sum).isEqualTo(2);
     }
 
     @Test
     public void delimiter_can_be_any_length() throws Exception {
-        assertThat(this.stringCalculator.add("//[aaa]\n1aaa2aaa3")).isEqualTo(6);
+        final int sum = this.stringCalculator.add("//[aaa]\n1aaa2aaa3");
+        assertThat(sum).isEqualTo(6);
     }
 
     @Ignore("too much regexp for tonight")
     @Test
     public void numbers_can_have_more_than_one_delimiter() throws Exception {
-        assertThat(this.stringCalculator.add("//[a][b]\n1a2b3")).isEqualTo(6);
+        final int sum = this.stringCalculator.add("//[a][b]\n1a2b3");
+        assertThat(sum).isEqualTo(6);
     }
 }
