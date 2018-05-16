f7a338b90d08e4a3b9780bd9549ca3ed763bf2ff
Lars Eckart <lars.eckart@gmail.com>
2018-05-16 22:17:21 +0300

    Tic Tac Toe, concept of players with different markers and partially concept of winning

--- src/main/java/ee/lars/tictactoe/Board.java
+++ src/main/java/ee/lars/tictactoe/Board.java
@@ -2,15 +2,47 @@ package ee.lars.tictactoe;
 
 public class Board {
 
-  private int[][] placedPieces = new int[3][3];
+  private char[][] placedPieces = new char[3][3];
+  private boolean isPlayerX = true;
 
   public void placePiece(int xAxis, int yAxis) {
-    if (xAxis > 3 || xAxis < 1 || yAxis > 3 || yAxis < 1) {
+    verifyXAxis(xAxis);
+    verifyYAxis(yAxis);
+    placePieceOnField(xAxis, yAxis);
+  }
+
+  private void verifyXAxis(int xAxis) {
+    if (xAxis > 3 || xAxis < 1) {
       throw new RuntimeException("x axis value too large");
     }
-    if (placedPieces[xAxis][yAxis] == 1) {
+  }
+
+  private void verifyYAxis(int yAxis) {
+    if (yAxis > 3 || yAxis < 1) {
+      throw new RuntimeException("y axis value too large");
+    }
+  }
+
+  private void placePieceOnField(int xAxis, int yAxis) {
+    if (placedPieces[xAxis - 1][yAxis - 1] == 'O' || placedPieces[xAxis - 1][yAxis - 1] == 'X') {
       throw new RuntimeException("space already occupied");
     }
-    placedPieces[xAxis][yAxis] = 1;
+    placedPieces[xAxis - 1][yAxis - 1] = isPlayerX ? 'X' : 'O';
+    isPlayerX = !isPlayerX;
+  }
+
+  public char getPlayerToken(int xAxis, int yAxis) {
+    return placedPieces[xAxis - 1][yAxis - 1];
+  }
+
+  public String getWinner() {
+
+    for (int i = 0; i < 3; i++) {
+      if (placedPieces[i][0] == 'X' && placedPieces[i][0] == 'X' && placedPieces[i][0] == 'X') {
+        return "X";
+      }
+    }
+    
+    return "no winner";
   }
 }
--- src/test/java/ee/lars/tictactoe/Board_should.java
+++ src/test/java/ee/lars/tictactoe/Board_should.java
@@ -3,49 +3,107 @@ package ee.lars.tictactoe;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Fail.fail;
 
 @RunWith(JUnitParamsRunner.class)
 public class Board_should {
 
+  private Board board;
+
+  @Before
+  public void setUp() throws Exception {
+    this.board = new Board();
+  }
+
   @Test
   @Parameters({"4", "5", "0"})
   public void not_allow_to_place_piece_outside_x_axis(int xAxis) throws Exception {
-    Board board = new Board();
     try {
       board.placePiece(xAxis, 0);
       fail("Placing a piece outside of x axis should have thrown exception");
     } catch (RuntimeException expected) {
-
+      assertThat(expected.getMessage()).isEqualToIgnoringCase("x axis value too large");
     }
   }
 
   @Test
   @Parameters({"4", "5", "0"})
   public void not_allow_to_place_piece_outside_y_axis(int yAxis) throws Exception {
-    Board board = new Board();
     try {
       board.placePiece(1, yAxis);
       fail("Placing a piece outside of y axis should have thrown exception");
     } catch (RuntimeException expected) {
-
+      assertThat(expected.getMessage()).isEqualToIgnoringCase("y axis value too large");
     }
   }
 
   @Test
   public void not_allow_to_place_piece_where_already_another_piece() throws Exception {
-    // given
-    Board board = new Board();
     board.placePiece(1, 1);
 
     try {
       board.placePiece(1, 1);
       fail("Placing a piece where already a piece was placed should have thrown exception");
     } catch (RuntimeException expected) {
-
+      assertThat(expected.getMessage()).isEqualToIgnoringCase("space already occupied");
     }
+  }
+
+  @Test
+  public void begin_new_game_with_player_x() throws Exception {
+    board.placePiece(1, 1);
+
+    char playerSign = board.getPlayerToken(1, 1);
+
+    assertThat(playerSign).isEqualTo('X');
+  }
+
+  @Test
+  public void make_next_turn_after_player_x_with_player_y() throws Exception {
+    board.placePiece(1, 1);
+    board.placePiece(2, 1);
+
+    char playerSign = board.getPlayerToken(2, 1);
+
+    assertThat(playerSign).isEqualTo('O');
+  }
+
+  @Ignore("false positive, was passing without changing implementation")
+  @Test
+  public void make_next_turn_after_player_y_with_player_x() throws Exception {
+    board.placePiece(1, 1);
+    board.placePiece(2, 1);
+    board.placePiece(3, 1);
+
+    char playerSign = board.getPlayerToken(3, 1);
+
+    assertThat(playerSign).isEqualTo('X');
+  }
+
+  @Test
+  public void have_no_winner_when_game_just_started() throws Exception {
+    String winner = board.getWinner();
+
+    // then
+    assertThat(winner).isEqualToIgnoringCase("no winner");
+  }
+
+  @Test
+  public void have_player_x_win_if_he_has_horizontal_line() throws Exception {
+    board.placePiece(1,1);
+    board.placePiece(1,2);
+    board.placePiece(2,1);
+    board.placePiece(1,3);
+    board.placePiece(3,1);
+
+    String winner = board.getWinner();
+
+    assertThat(winner).isEqualToIgnoringCase("X");
   }
 }
