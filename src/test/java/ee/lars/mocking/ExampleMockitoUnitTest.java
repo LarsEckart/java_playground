bac9252d9cc88bf190c2eb0d90533250d1377d58
Lars Eckart <lars.eckart@gmail.com>
2018-06-05 22:19:30 +0300

    remove unused and unavailable jmockit imports, shame on me

--- src/test/java/ee/lars/mocking/ExampleMockitoUnitTest.java
+++ src/test/java/ee/lars/mocking/ExampleMockitoUnitTest.java
@@ -1,9 +1,6 @@
 package ee.lars.mocking;
 
-import mockit.Expectations;
-import mockit.FullVerifications;
-import mockit.Mocked;
-import mockit.Verifications;
+import java.util.ArrayList;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -11,56 +8,49 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public class ExampleMockitoUnitTest {
 
-    @Rule public MockitoRule rule = MockitoJUnit.rule();
+  @Rule public MockitoRule rule = MockitoJUnit.rule();
 
-    @Mock
-    private Collaborator collaborator;
+  @Mock
+  private Collaborator collaborator;
 
-    @InjectMocks
-    private Performer performer;
+  @InjectMocks
+  private Performer performer;
 
-    @Test
-    public void testThePerformMethod() {
-        Model model = new Model();
-        given(this.collaborator.collaborate(model.getInfo())).willReturn(true);
+  @Test
+  public void testThePerformMethod() {
+    Model model = new Model();
+    given(this.collaborator.collaborate(model.getInfo())).willReturn(true);
 
-        performer.perform(model);
+    performer.perform(model);
 
-        verify(this.collaborator).receive(true);
-    }
+    verify(this.collaborator).receive(true);
+  }
 
-    @Mock
-    ExpectationsCollaborator mock;
+  @Mock
+  ExpectationsCollaborator mock;
 
-    @Test
-    public void verify_primitives() throws Exception {
-        given(mock.methodForAny1(anyString(), anyInt(), anyBoolean())).willReturn("any");
+  @Test
+  public void verify_primitives() throws Exception {
+    given(mock.methodForAny1(anyString(), anyInt(), anyBoolean())).willReturn("any");
 
-        assertEquals("any", mock.methodForAny1("barfooxyz", 0, Boolean.FALSE));
+    assertEquals("any", mock.methodForAny1("barfooxyz", 0, Boolean.FALSE));
+  }
 
+  @Test
+  public void verify_list() throws Exception {
+    mock.methodForAny2(2L, new ArrayList<>());
 
-    }
-
-    @Test
-    public void verify_list() throws Exception {
-        mock.methodForAny2(2L, new ArrayList<>());
-
-        verify(mock).methodForAny2(anyLong(), anyList());
-    }
+    verify(mock).methodForAny2(anyLong(), anyList());
+  }
 }
