87b7909aee2078ac51320f02768d2bd71868ba12
Lars Eckart <lars.eckart@gmail.com>
2018-07-14 18:39:04 +0300

    dependency updates

--- src/main/java/lars/OptionalPattern.java
+++ src/main/java/lars/OptionalPattern.java
@@ -2,14 +2,23 @@ package lars;
 
 import java.util.Optional;
 
+/**
+ * http://blog.joda.org/2015/09/naming-optional-query-methods.html
+ */
 public interface OptionalPattern<T> {
 
+    /**
+     * return empty optional if no attribute
+     */
     public abstract Optional<T> findValue(long id);
 
     public default boolean containsValue(long id) {
         return findValue(id).isPresent();
     }
 
+    /**
+     * throw exception if no attribute
+     */
     public default T getValue(long id) {
         return findValue(id).orElseThrow(() -> new RuntimeException("boom"));
     }
--- src/main/java/lars/OptionalProperties.java
+++ src/main/java/lars/OptionalProperties.java
@@ -11,13 +11,12 @@ public class OptionalProperties {
         return Optional.ofNullable(properties.get(propertyName));
     }
 
-    public <T> Optional<T> getProperty(
-            String propertyName, ThrowingFunction<String, ? extends T, ? extends Exception> supplier) {
+    public <T> Optional<T> getProperty(String propertyName, ThrowingFunction<String, ? extends T, ? extends Exception> supplier) {
         return this.getProperty(propertyName).map(val -> {
             try {
                 return supplier.apply(val);
             } catch (Exception e) {
-                System.err.println("Invalid property transform, will default " + e.getMessage());
+                System.err.println("Invalid property transform, will default." + e.getMessage());
                 return null;
             }
         });
@@ -29,6 +28,7 @@ public class OptionalProperties {
         boolean enabled = config.getProperty("cache.enabled", Boolean::getBoolean).orElse(true);
     }
 
+    @FunctionalInterface
     interface ThrowingFunction<T, R, E extends Throwable> {
 
         R apply(T arg) throws E;
