f7b904ce8fb6a5e6a8002041728a3d0daea408bf
Lars Eckart <lars.eckart@gmail.com>
2018-07-24 23:31:12 +0300

    String.codePoints(), interesting method!

---
+++ src/test/java/lars/Strings.java
@@ -0,0 +1,49 @@
+package lars;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Strings {
+
+    @Test
+    public void codePoints_to_string_list_with_Character_toString() throws Exception {
+        String name = "Lars";
+
+        List<String> collect = name.codePoints()
+                .mapToObj(cp -> Character.toString((char) cp))
+                .collect(Collectors.toList());
+
+        assertThat(collect).isEqualTo(List.of("L", "a", "r", "s"));
+    }
+
+    @Test
+    public void codePoints_to_string_list_with_special_trick() throws Exception {
+        String name = "Lars";
+
+        List<String> collect = name.codePoints().mapToObj(cp -> "" + (char) cp).collect(Collectors.toList());
+
+        assertThat(collect).isEqualTo(List.of("L", "a", "r", "s"));
+    }
+
+    @Test
+    public void codePoints_with_numbers() throws Exception {
+        String name = "1337";
+
+        List<String> collect = name.codePoints().mapToObj(cp -> Character.toString((char) cp)).collect(Collectors.toList());
+
+        assertThat(collect).isEqualTo(List.of("1", "3", "3", "7"));
+    }
+
+    @Test
+    public void chars_to_string_list_with_Character_toString() throws Exception {
+        String name = "Lars";
+
+        List<String> collect = name.chars().mapToObj(c -> Character.toString((char) c)).collect(Collectors.toList());
+
+        assertThat(collect).isEqualTo(List.of("L", "a", "r", "s"));
+    }
+}
