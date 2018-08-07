01206ca498c01f0ba134bda8870fafd26a72d670
Lars Eckart <lars.eckart@proekspert.ee>
2018-08-07 21:23:09 +0300

    green: requirement 2

--- src/main/java/lars/katas/Greeter.java
+++ src/main/java/lars/katas/Greeter.java
@@ -3,6 +3,10 @@ package lars.katas;
 public class Greeter {
 
     public String greet(String name) {
+        if (name == null) {
+            name = "my friend";
+            return String.format("Hello, %s.", name);
+        }
         return String.format("Hello, %s.", name);
     }
 }
