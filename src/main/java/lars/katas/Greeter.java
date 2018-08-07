2f8610ce7b7c388f558f8bd9050c7612279cd6fc
Lars Eckart <lars.eckart@proekspert.ee>
2018-08-07 21:43:40 +0300

    green: requirement 5

--- src/main/java/lars/katas/Greeter.java
+++ src/main/java/lars/katas/Greeter.java
@@ -1,19 +1,37 @@
 package lars.katas;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Greeter {
 
-    public String greet(String... name) {
-        if (name == null) {
+    public String greet(String... names) {
+        if (names == null) {
             return "Hello, my friend.";
         }
-        if (isSingleName(name)) {
-            if (isAllUpperCase(name[0])) {
-                return String.format("HELLO %s!", name[0].toUpperCase());
+        if (isSingleName(names)) {
+            if (isAllUpperCase(names[0])) {
+                return String.format("HELLO %s!", names[0].toUpperCase());
             }
-            return String.format("Hello, %s.", name[0]);
+            return String.format("Hello, %s.", names[0]);
         }
 
-        return String.format("Hello, %s and %s.", name[0], name[1]);
+        if (isTwoNames(names)) {
+            return String.format("Hello, %s and %s.", names[0], names[1]);
+        }
+
+        List<String> allNames = List.of(names);
+        List<String> commaSeparatedNames = allNames.subList(0, allNames.size() - 1);
+
+        String collect = String.join(", ", commaSeparatedNames);
+
+
+        return String.format("Hello, %s and %s.", collect, names[names.length - 1]);
+    }
+
+
+    private boolean isTwoNames(String[] names) {
+        return names.length == 2;
     }
 
     private boolean isSingleName(String[] name) {
