1ca96778f8378d0c08e6131d5d3bad53e16dba91
Lars Eckart <lars.eckart@proekspert.ee>
2018-08-07 21:33:47 +0300

    green: requirement 4

--- src/main/java/lars/katas/Greeter.java
+++ src/main/java/lars/katas/Greeter.java
@@ -6,10 +6,18 @@ public class Greeter {
         if (name == null) {
             return "Hello, my friend.";
         }
-        if (isAllUpperCase(name[0])) {
-            return String.format("HELLO %s!", name[0].toUpperCase());
+        if (isSingleName(name)) {
+            if (isAllUpperCase(name[0])) {
+                return String.format("HELLO %s!", name[0].toUpperCase());
+            }
+            return String.format("Hello, %s.", name);
         }
-        return String.format("Hello, %s.", name);
+
+        return String.format("Hello, %s and %s.", name[0], name[1]);
+    }
+
+    private boolean isSingleName(String[] name) {
+        return name.length == 1;
     }
 
     private boolean isAllUpperCase(String name) {
