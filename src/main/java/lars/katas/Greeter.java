ab31bbddfa4550074f60fd53706fa861f12b8097
Lars Eckart <lars.eckart@proekspert.ee>
2018-08-07 21:24:55 +0300

    refactor: requirement 2

--- src/main/java/lars/katas/Greeter.java
+++ src/main/java/lars/katas/Greeter.java
@@ -5,7 +5,6 @@ public class Greeter {
     public String greet(String name) {
         if (name == null) {
             name = "my friend";
-            return String.format("Hello, %s.", name);
         }
         return String.format("Hello, %s.", name);
     }
--- src/test/java/lars/katas/Greeter_should.java
+++ src/test/java/lars/katas/Greeter_should.java
@@ -1,16 +1,21 @@
 package lars.katas;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Greeter_should {
 
+    private Greeter greeter;
+
+    @Before
+    public void setUp() throws Exception {
+        greeter = new Greeter();
+    }
+
     @Test
     public void greet_with_given_name() throws Exception {
-        // given
-        Greeter greeter = new Greeter();
-
         // when
         String greeting = greeter.greet("Bob");
 
@@ -20,9 +25,6 @@ public class Greeter_should {
 
     @Test
     public void greet_with_default_greeting_when_given_null() throws Exception {
-        // given
-        Greeter greeter = new Greeter();
-
         // when
         String greeting = greeter.greet(null);
 
