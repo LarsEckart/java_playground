d2ee36f7de6d6806763cf00d7f79199e073c2423
Lars Eckart <lars.eckart@gmail.com>
2018-10-27 18:07:41 +0300

    refactor: salary slip iteration 1, trying out some junit5

--- src/test/java/lars/katas/salaryslip/SalarySlipGenerator_should.java
+++ src/test/java/lars/katas/salaryslip/SalarySlipGenerator_should.java
@@ -1,8 +1,11 @@
 package lars.katas.salaryslip;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -15,11 +18,12 @@ class SalarySlipGenerator_should {
 
     private SalarySlipGenerator salarySlipGenerator;
 
-    @Test
-    void calculate_monthly_gross_salary() {
+    @ParameterizedTest
+    @MethodSource("grossSalaryProvider")
+    void calculate_monthly_gross_salary(BigDecimal annualGrossSalary, BigDecimal monthlyGrossSalary) {
         // given
         salarySlipGenerator = new SalarySlipGenerator();
-        Employee employee = new Employee(ANY_ID, ANY_NAME, BigDecimal.valueOf(5_000.00));
+        Employee employee = employeeWithAnnualSalaryOf(annualGrossSalary);
 
         // when
         SalarySlip salarySlip = salarySlipGenerator.generateFor(employee);
@@ -28,7 +32,18 @@ class SalarySlipGenerator_should {
         assertAll(
             () -> assertThat(salarySlip.getEmployeeId()).isEqualTo(ANY_ID),
             () -> assertThat(salarySlip.getEmployeeName()).isEqualTo(ANY_NAME),
-            () -> assertThat(salarySlip.getMonthlyGrossSalary()).isEqualTo(EXPECTED_MONTHLY_GROSS_SALARY)
+            () -> assertThat(salarySlip.getMonthlyGrossSalary()).isEqualTo(monthlyGrossSalary)
+        );
+    }
+
+    private Employee employeeWithAnnualSalaryOf(BigDecimal annualGrossSalary) {
+        return new Employee(ANY_ID, ANY_NAME, annualGrossSalary);
+    }
+
+    static Stream<Arguments> grossSalaryProvider() {
+        return Stream.of(
+            Arguments.of(BigDecimal.valueOf(5_000.000), EXPECTED_MONTHLY_GROSS_SALARY),
+            Arguments.of(BigDecimal.valueOf(9_060.00), BigDecimal.valueOf(755.00).setScale(2))
         );
     }
 }
