f59e04211e6f7e8c4f23caadd651d7d6dabc653e
Lars Eckart <lars.eckart@gmail.com>
2018-11-22 14:39:42 +0100

    approval testing gilded rose

--- src/test/java/lars/katas/gildedrose/GildedRoseTest.java
+++ src/test/java/lars/katas/gildedrose/GildedRoseTest.java
@@ -1,16 +1,23 @@
 package lars.katas.gildedrose;
 
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
 
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] {new Item("foo", 0, 0)};
+    public void foo() throws Exception {
+
+        CombinationApprovals.verifyAllCombinations(this::doUpdateQuality,
+                new String[] {"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"},
+                new Integer[] {-1, 0, 2, 8, 11},
+                new Integer[] {0, 1, 49, 50});
+    }
+
+    private String doUpdateQuality(String name, int sellIn, int quality) {
+        Item[] items = new Item[] {new Item(name, sellIn, quality)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        return app.items[0].toString();
     }
 }
