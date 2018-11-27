3aba972eef91f52a2636701b83a8c0c0f472e4dd
Lars Eckart <lars.eckart@gmail.com>
2018-11-27 10:18:42 +0100

    mockito extension to stub in 1 line

---
+++ src/test/java/lars/MockitoExamples.java
@@ -0,0 +1,33 @@
+package lars;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+
+import static lars.MockitoExtension.stub;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class MockitoExamples {
+
+    @Test
+    void any() {
+        LinkedList mockedList = mock(LinkedList.class);
+        when(mockedList.get(0)).thenReturn("first");
+
+        assertThat(mockedList.get(0)).isEqualTo("first");
+
+        verify(mockedList).get(0);
+    }
+
+    @Test
+    void improved() {
+        LinkedList mockedList = stub(LinkedList.class, l -> l.get(0), "first");
+
+        assertThat(mockedList.get(0)).isEqualTo("first");
+
+        verify(mockedList).get(0);
+    }
+}
---
+++ src/test/java/lars/MockitoExtension.java
@@ -0,0 +1,20 @@
+package lars;
+
+import java.util.function.Function;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class MockitoExtension {
+
+    public static <T> T stub(Class<T> klass, Function<T, Object> when, Object returnValue) {
+        try {
+            T result = mock(klass);
+            when(when.apply(result)).thenReturn(returnValue);
+            return result;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
