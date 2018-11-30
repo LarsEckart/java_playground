77c02e3ace85fb55e6a7369d565b2eac623f7695
Lars Eckart <lars.eckart@gmail.com>
2018-11-30 15:01:49 +0100

    update gradle wrapper to 5.0

--- src/main/java/lars/spielplatz/ModelReader.java
+++ src/main/java/lars/spielplatz/ModelReader.java
@@ -1,61 +1,61 @@
 package lars.spielplatz;
 
-import java.util.HashMap;
-import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class ModelReader {
 
     public Map<String, TreeSet<String>> getEntitiesByPackage(
             Set<String> packageFilterSet, Set<String> entityFilterSet) {
-
-        Map<String, TreeSet<String>> entitiesByPackage = new HashMap<>();
-        //put the entityNames TreeSets in a HashMap by packageName
-        Iterator<String> ecIter = this.getEntityNames().iterator();
-        while (ecIter.hasNext()) {
-            String entityName = ecIter.next();
-            ModelEntity entity = this.getModelEntity(entityName);
-            String packageName = entity.getPackageName();
-            if (!packageFilterSet.isEmpty()) {
-                // does it match any of these?
-                boolean foundMatch = false;
-                for (String packageFilter : packageFilterSet) {
-                    if (packageName.contains(packageFilter)) {
-                        foundMatch = true;
-                    }
-                }
-                if (!foundMatch) {
-                    continue;
-                }
-            }
-            if (!entityFilterSet.isEmpty()
-                    && !entityFilterSet.contains(entityName)) {
-                continue;
-            }
-            TreeSet<String> entities = entitiesByPackage.get(packageName);
-            if (entities == null) {
-                entities = new TreeSet<>();
-                entitiesByPackage.put(packageName, entities);
-            }
-            entities.add(entityName);
-        }
-        return entitiesByPackage;
+        return this.getEntityNames().stream()
+                .map(_entityName -> new Object() { // anonymous inner class
+                    String entityName = _entityName; // need to redeclare it
+                    ModelEntity entity = getModelEntity(_entityName);
+                    String packageName = entity.getPackageName();
+                })
+                .filter(tuple -> matchesPackageFilter(
+                        packageFilterSet, tuple.packageName))
+                .filter(tuple -> matchesEntityFilter(
+                        entityFilterSet, tuple.entityName))
+                .collect(Collectors.groupingBy(tuple -> tuple.packageName,
+                        Collectors.mapping(tuple -> tuple.entityName,
+                                Collectors.toCollection(TreeSet::new))));
     }
 
+    private boolean matchesPackageFilter(
+            Set<String> packageFilterSet, String packageName) {
+        return UtilValidate.isEmpty(packageFilterSet) ||
+                packageFilterSet.stream()
+                        .anyMatch(packageName::contains);
+    }
+
+    private boolean matchesEntityFilter(
+            Set<String> entityFilterSet, String entityName) {
+        return UtilValidate.isEmpty(entityFilterSet) ||
+                entityFilterSet.contains(entityName);
+    }
 
     private ModelEntity getModelEntity(String entityName) {
         throw new UnsupportedOperationException("implement me!");
     }
 
-    private Iterable<String> getEntityNames() {
+    private List<String> getEntityNames() {
         throw new UnsupportedOperationException("implement me!");
     }
 
     private class ModelEntity {
 
         public String getPackageName() {
+            throw new UnsupportedOperationException("implement me!");
+        }
+    }
+
+    private static class UtilValidate {
+
+        public static boolean isEmpty(Set<String> packageFilterSet) {
             throw new UnsupportedOperationException("implement me!");
         }
     }
