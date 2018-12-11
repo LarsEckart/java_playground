30ed90dff468c33a061f6fb11ba335476ee2155d
Lars Eckart <lars.eckart@gmail.com>
2018-12-11 15:52:02 +0200

    progressing with 99 bottles

--- src/main/java/lars/katas/bottles/ShamelessGreenBottles.java
+++ src/main/java/lars/katas/bottles/ShamelessGreenBottles.java
@@ -10,20 +10,23 @@ public class ShamelessGreenBottles extends Bottles {
                         "no more bottles of beer.\n" +
                         "Go to the store and buy some more, " +
                         "99 bottles of beer on the wall.\n";
-            case 1:
-                return number + " " + container(number) + " of beer on the wall, " +
-                        number + " " + container(number) + " of beer.\n" +
-                        "Take it down and pass it around, " +
-                        "no more bottles of beer on the wall.\n";
             default:
                 return number + " " + container(number) + " of beer on the wall, " +
                         number + " " + container(number) + " of beer.\n" +
-                        "Take one down and pass it around, " +
-                        (number - 1) + " " + container(number - 1) + " of beer on the wall.\n";
+                        "Take " + pronoun(number) + " down and pass it around, " +
+                        quantity(number - 1) + " " + container(number - 1) + " of beer on the wall.\n";
         }
     }
 
     private String container(int number) {
         return number == 1 ? "bottle" : "bottles";
+    }
+
+    private String pronoun(int number) {
+        return number == 1 ? "it" : "one";
+    }
+
+    private String quantity(int number) {
+        return number == 0 ? "no more" : String.valueOf(number);
     }
 }
