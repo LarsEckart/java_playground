8c790b5bbc5a17e0c82857454bff27e8cf91c543
Lars Eckart <lars.eckart@gmail.com>
2018-12-11 16:30:30 +0200

    oh my god, 99 bottles flocked

--- src/main/java/lars/katas/bottles/ShamelessGreenBottles.java
+++ src/main/java/lars/katas/bottles/ShamelessGreenBottles.java
@@ -4,18 +4,14 @@ public class ShamelessGreenBottles extends Bottles {
 
     @Override
     public String verse(int number) {
-        switch (number) {
-            case 0:
-                return "No more bottles of beer on the wall, " +
-                        "no more bottles of beer.\n" +
-                        "Go to the store and buy some more, " +
-                        "99 bottles of beer on the wall.\n";
-            default:
-                return number + " " + container(number) + " of beer on the wall, " +
-                        number + " " + container(number) + " of beer.\n" +
-                        "Take " + pronoun(number) + " down and pass it around, " +
-                        quantity(number - 1) + " " + container(number - 1) + " of beer on the wall.\n";
-        }
+        return capitalize(quantity(number)) + " " + container(number) + " of beer on the wall, " +
+                quantity(number) + " " + container(number) + " of beer.\n" +
+                action(number) + ", " +
+                quantity(successor(number)) + " " + container(successor(number)) + " of beer on the wall.\n";
+    }
+
+    private int successor(int number) {
+        return number == 0 ? 99 : number - 1;
     }
 
     private String container(int number) {
@@ -28,5 +24,13 @@ public class ShamelessGreenBottles extends Bottles {
 
     private String quantity(int number) {
         return number == 0 ? "no more" : String.valueOf(number);
+    }
+
+    private String action(int number) {
+        return number == 0 ? "Go to the store and buy some more" : "Take " + pronoun(number) + " down and pass it around";
+    }
+
+    private String capitalize(String text) {
+        return text.substring(0, 1).toUpperCase() + text.substring(1);
     }
 }
