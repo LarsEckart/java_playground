4ea65f1c79bd5a5a2245de82bd3a0d63db822dca
Lars Eckart <lars.eckart@gmail.com>
2018-12-12 13:39:15 +0200

    finished refactoring 99 bottles

--- src/main/java/lars/katas/bottles/OpenBottles.java
+++ src/main/java/lars/katas/bottles/OpenBottles.java
@@ -5,10 +5,12 @@ import java.util.stream.Stream;
 
 public class OpenBottles extends Bottles {
 
+    @Override
     public String song() {
         return verses(99, 0);
     }
 
+    @Override
     public String verses(int start, int end) {
         return Stream.iterate(start, i -> i >= end, i -> i - 1)
                 .map(this::verse)
@@ -17,50 +19,141 @@ public class OpenBottles extends Bottles {
 
     @Override
     public String verse(int number) {
-        BottleNumber bottleNumber = new BottleNumber(number);
-        BottleNumber nextBottleNumber = bottleNumber.successor();
+        BottleNumber bottleNumber = BottleNumber.createFor(number);
 
         return capitalize(bottleNumber + " of beer on the wall, ") +
                 bottleNumber + " of beer.\n" +
                 bottleNumber.action() + ", " +
-                nextBottleNumber + " of beer on the wall.\n";
+                bottleNumber.successor() + " of beer on the wall.\n";
     }
 
     private String capitalize(String text) {
         return text.substring(0, 1).toUpperCase() + text.substring(1);
     }
 
-    class BottleNumber {
+    static class BottleNumber {
 
         private int number;
 
-        public BottleNumber(int number) {
+        BottleNumber(int number) {
             this.number = number;
         }
 
-        private BottleNumber successor() {
-            return this.number == 0 ? new BottleNumber(99) : new BottleNumber(number - 1);
+        static BottleNumber createFor(int number) {
+            switch (number) {
+                case 0:
+                    BottleNumber0 bottleNumber0 = new BottleNumber0(number);
+                    System.out.println(bottleNumber0.getClass().getName());
+                    return bottleNumber0;
+                case 1:
+                    return new BottleNumber1(number);
+                case 6:
+                    return new BottleNumber6(number);
+                default:
+                    return new BottleNumber(number);
+            }
+
+            /*
+            Class c;
+            try {
+                c = Class.forName("lars.katas.bottles.OpenBottles$BottleNumber" + number);
+            } catch (ClassNotFoundException e) {
+                try {
+                    c = Class.forName("lars.katas.bottles.OpenBottles$BottleNumber");
+                } catch (ClassNotFoundException e1) {
+                    throw new RuntimeException("out of luck");
+                }
+            }
+            Class[] type = { int.class };
+            Constructor constructor = null;
+            try {
+                constructor = c.getDeclaredConstructor(type);
+                return (BottleNumber) constructor.newInstance(number);
+            } catch (NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
+                throw new RuntimeException("out of luck");
+            }
+            */
         }
 
-        private String container() {
-            return number == 1 ? "bottle" : "bottles";
+        protected BottleNumber successor() {
+            return createFor(number - 1);
         }
 
-        private String pronoun() {
-            return number == 1 ? "it" : "one";
+        protected String container() {
+            return "bottles";
         }
 
-        private String quantity() {
-            return number == 0 ? "no more" : String.valueOf(number);
+        protected String pronoun() {
+            return "one";
         }
 
-        private String action() {
-            return number == 0 ? "Go to the store and buy some more" : "Take " + pronoun() + " down and pass it around";
+        protected String quantity() {
+            return String.valueOf(number);
+        }
+
+        protected String action() {
+            return "Take " + pronoun() + " down and pass it around";
         }
 
         @Override
         public String toString() {
             return quantity() + " " + container();
+        }
+    }
+
+    static class BottleNumber0 extends BottleNumber {
+
+        BottleNumber0(int number) {
+            super(number);
+        }
+
+        @Override
+        protected String quantity() {
+            return "no more";
+        }
+
+        @Override
+        protected BottleNumber successor() {
+            return createFor(99);
+        }
+
+        @Override
+        protected String action() {
+            return "Go to the store and buy some more";
+        }
+    }
+
+    static class BottleNumber1 extends BottleNumber {
+
+        BottleNumber1(int number) {
+            super(number);
+        }
+
+        @Override
+        protected String pronoun() {
+            return "it";
+        }
+
+        @Override
+        protected String container() {
+            return "bottle";
+        }
+    }
+
+    static class BottleNumber6 extends BottleNumber {
+
+        BottleNumber6(int number) {
+            super(number);
+        }
+
+        @Override
+        protected String container() {
+            return "six-pack";
+        }
+
+        @Override
+        protected String quantity() {
+            return "1";
         }
     }
 }
