10f29aba3889fb1b5e11ac843fdcec5276253a23
Lars Eckart <lars.eckart@gmail.com>
2018-12-13 10:27:45 +0200

    extract method

--- src/main/java/lars/refactoring/SplitPhase.java
+++ src/main/java/lars/refactoring/SplitPhase.java
@@ -10,21 +10,25 @@ public class SplitPhase {
 
     public static void main(String[] args) {
         try {
-            if (args.length == 0) {
-                throw new RuntimeException("must supply a filename");
-            }
-            String filename = args[args.length - 1];
-            File input = Paths.get(filename).toFile();
-            ObjectMapper mapper = new ObjectMapper();
-            Order[] orders = mapper.readValue(input, Order[].class);
-            if (Stream.of(args).anyMatch(arg -> "-r".equals(arg))) {
-                System.out.println(Stream.of(orders).filter(o -> "ready".equals(o.status)).count());
-            } else {
-                System.out.println(orders.length);
-            }
+            run(args);
         } catch (Exception e) {
             System.err.println(e);
             System.exit(1);
+        }
+    }
+
+    private static void run(String[] args) throws java.io.IOException {
+        if (args.length == 0) {
+            throw new RuntimeException("must supply a filename");
+        }
+        String filename = args[args.length - 1];
+        File input = Paths.get(filename).toFile();
+        ObjectMapper mapper = new ObjectMapper();
+        Order[] orders = mapper.readValue(input, Order[].class);
+        if (Stream.of(args).anyMatch(arg -> "-r".equals(arg))) {
+            System.out.println(Stream.of(orders).filter(o -> "ready".equals(o.status)).count());
+        } else {
+            System.out.println(orders.length);
         }
     }
 
