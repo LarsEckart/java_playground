4c844f9ccdd0088a9278ca3c5f9cd5c269bd6c11
Lars Eckart <lars.eckart@gmail.com>
2018-12-25 12:36:59 +0200

    refactoring: move and rename method

--- src/main/java/lars/refactoring/documentload/Assortment.java
+++ src/main/java/lars/refactoring/documentload/Assortment.java
@@ -1,5 +1,6 @@
 package lars.refactoring.documentload;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
@@ -9,5 +10,13 @@ class Assortment {
 
     public List<Album> getAlbums() {
         return Collections.unmodifiableList(albums);
+    }
+
+    static Assortment fromJson(String json)  {
+        try {
+            return Json.mapper().readValue(json, Assortment.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/lars/refactoring/documentload/Service.java
+++ src/main/java/lars/refactoring/documentload/Service.java
@@ -1,25 +1,15 @@
 package lars.refactoring.documentload;
 
-import java.io.IOException;
-
 class Service {
 
     private DataSource dataSource;
 
     public String tuesdayMusic(String query) {
         try {
-            Assortment data = loadAssortment(dataSource.getAlbumList(query));
+            Assortment data = Assortment.fromJson(dataSource.getAlbumList(query));
             return Json.mapper().writeValueAsString(data);
         } catch (Exception e) {
             System.out.println(e);
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static Assortment loadAssortment(String json)  {
-        try {
-            return Json.mapper().readValue(json, Assortment.class);
-        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
