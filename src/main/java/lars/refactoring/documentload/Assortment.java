80e24bd76341613076232252cef748f887f3f735
Lars Eckart <lars.eckart@gmail.com>
2018-12-25 12:52:37 +0200

    Passing through the JSON
    even can then remove the album and track classes!

--- src/main/java/lars/refactoring/documentload/Assortment.java
+++ src/main/java/lars/refactoring/documentload/Assortment.java
@@ -1,32 +1,16 @@
 package lars.refactoring.documentload;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
-
 class Assortment {
 
-    private List<Album> albums;
+    private String json;
 
-    public List<Album> getAlbums() {
-        return Collections.unmodifiableList(albums);
+    public static Assortment fromJson(String json) {
+        Assortment result = new Assortment();
+        result.json = json;
+        return result;
     }
 
-    static Assortment fromJson(String json)  {
-        try {
-            return Json.mapper().readValue(json, Assortment.class);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    String toJson() {
-        try {
-            return Json.mapper().writeValueAsString(this);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+    public String toJson() {
+        return json;
     }
 }
