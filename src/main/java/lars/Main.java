828b7cd5394f5bc9498710ca4d38c990ac3f9f06
Lars Eckart <lars.eckart@gmail.com>
2019-05-06 11:03:39 +0300

    revert few things

--- src/main/java/lars/Main.java
+++ src/main/java/lars/Main.java
@@ -4,6 +4,7 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 public class Main {
+
     private static final BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(15);
 
     public static void main(String[] args) {
@@ -14,13 +15,12 @@ public class Main {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-
             }
         });
         t.start();
 
         for (int i = 0; i < 10; i++) {
-            final int j =i;
+            final int j = i;
             queue.add(() -> {
                 System.out.println("Hello" + j);
             });
