8d559d8ded07ac61707900fc286fc5b46052f51c
Lars Eckart <lars.eckart@gmail.com>
2019-05-06 12:06:35 +0300

    refactor tests to avoid duplication

--- src/test/java/lars/tddpracticalguide/TestMovieList.java
+++ src/test/java/lars/tddpracticalguide/TestMovieList.java
@@ -1,32 +1,38 @@
 package lars.tddpracticalguide;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class TestMovieList {
 
+    private MovieList movieList;
+    private Movie starWars;
+    private Movie starTrek;
+
+    @BeforeEach
+    void setUp() {
+        movieList = new MovieList();
+        starWars = new Movie();
+        starTrek = new Movie();
+    }
+
     @Test
     void empty_list_size_should_be_zero() {
-        MovieList emptyList = new MovieList();
-        assertThat(emptyList.size()).isEqualTo(0);
+        assertThat(movieList.size()).isEqualTo(0);
     }
 
     @Test
     void list_has_size_of_one_after_adding_one_movie() {
-        Movie starWars = new Movie();
-        MovieList oneItemList = new MovieList();
-        oneItemList.add(starWars);
-        assertThat(oneItemList.size()).isEqualTo(1);
+        movieList.add(starWars);
+        assertThat(movieList.size()).isEqualTo(1);
     }
 
     @Test
     void list_has_size_of_two_after_adding_two_movies() {
-        Movie starWars = new Movie();
-        Movie starTrek = new Movie();
-        MovieList oneItemList = new MovieList();
-        oneItemList.add(starWars);
-        oneItemList.add(starTrek);
-        assertThat(oneItemList.size()).isEqualTo(2);
+        movieList.add(starWars);
+        movieList.add(starTrek);
+        assertThat(movieList.size()).isEqualTo(2);
     }
 }
