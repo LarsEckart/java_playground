65f162a4be2f17d25ee7c87a7bc0f44f3cb9a04f
Lars Eckart <lars.eckart@gmail.com>
2019-08-23 20:38:58 +0300

    fun with emojis

---
+++ src/main/java/lars/MysteryAnimal.java
@@ -0,0 +1,20 @@
+package lars;
+
+public class MysteryAnimal {
+
+    public static void main(String[] args) {
+        String bear = "🐻";
+
+        // If the previous line doesn't show up in your editor,
+        // you can comment it out and use this declaration instead:
+        // String bear = "\ud83d\udc3b";
+
+        int bearCodepoint = bear.codePointAt(bear.offsetByCodePoints(0, 0));
+        int mysteryAnimalCodepoint = bearCodepoint + 1;
+
+        char mysteryAnimal[] = {Character.highSurrogate(mysteryAnimalCodepoint),
+                Character.lowSurrogate(mysteryAnimalCodepoint)};
+        System.out.println("The Coderland Zoo's latest attraction: "
+                + String.valueOf(mysteryAnimal));
+    }
+}
