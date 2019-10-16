390cd0d87fe7a31d4d8434d0769ceca250a9a833
Lars Eckart <lars.eckart@gmail.com>
2019-10-16 22:55:51 +0300

    try google-code-format intellij plugin

--- src/main/java/lars/advent2017/ChecksumCalculator.java
+++ src/main/java/lars/advent2017/ChecksumCalculator.java
@@ -5,43 +5,43 @@ import java.util.List;
 
 public class ChecksumCalculator {
 
-    public int checksum(String matrix) {
-        List<String> rows = List.of(matrix.split("\n"));
+  public int checksum(String matrix) {
+    List<String> rows = List.of(matrix.split("\n"));
 
-        int sum = 0;
-        for (String row : rows) {
-            int max = Arrays.stream(row.split("\t")).mapToInt(Integer::parseInt).max().getAsInt();
-            int min = Arrays.stream(row.split("\t")).mapToInt(Integer::parseInt).min().getAsInt();
-            int diff = max - min;
-            sum += diff;
-        }
-        return sum;
+    int sum = 0;
+    for (String row : rows) {
+      int max = Arrays.stream(row.split("\t")).mapToInt(Integer::parseInt).max().getAsInt();
+      int min = Arrays.stream(row.split("\t")).mapToInt(Integer::parseInt).min().getAsInt();
+      int diff = max - min;
+      sum += diff;
     }
+    return sum;
+  }
 
-    public int divisableChecksum(String matrix) {
-        List<String> rows = List.of(matrix.split("\n"));
+  public int divisableChecksum(String matrix) {
+    List<String> rows = List.of(matrix.split("\n"));
 
-        int sum = 0;
-        for (String row : rows) {
-            String[] rowItems = row.split("\t");
-            for (String item : rowItems) {
-                int number = Integer.parseInt(item);
+    int sum = 0;
+    for (String row : rows) {
+      String[] rowItems = row.split("\t");
+      for (String item : rowItems) {
+        int number = Integer.parseInt(item);
 
-                int index = 0;
-                while (index < rowItems.length) {
-                    int otherNumber = Integer.parseInt(rowItems[index]);
-                    if (otherNumber != number) {
-                        int res = number % otherNumber;
+        int index = 0;
+        while (index < rowItems.length) {
+          int otherNumber = Integer.parseInt(rowItems[index]);
+          if (otherNumber != number) {
+            int res = number % otherNumber;
 
-                        if (res == 0) {
-                            int diff = number / otherNumber;
-                            sum += diff;
-                        }
-                    }
-                    index++;
-                }
+            if (res == 0) {
+              int diff = number / otherNumber;
+              sum += diff;
             }
+          }
+          index++;
         }
-        return sum;
+      }
     }
+    return sum;
+  }
 }
--- src/main/java/lars/advent2017/SequenceSumCalculator.java
+++ src/main/java/lars/advent2017/SequenceSumCalculator.java
@@ -5,34 +5,34 @@ import java.util.stream.Collectors;
 
 public class SequenceSumCalculator {
 
-    public int sum(String captcha) {
-        List<Integer> digits = captcha.chars().map(i -> i - 48).boxed().collect(Collectors.toList());
-        return sumSequenceJumps(digits, 1);
+  public int sum(String captcha) {
+    List<Integer> digits = captcha.chars().map(i -> i - 48).boxed().collect(Collectors.toList());
+    return sumSequenceJumps(digits, 1);
+  }
+
+  public int sumHalf(String captcha) {
+    List<Integer> digits = captcha.chars().map(i -> i - 48).boxed().collect(Collectors.toList());
+    return sumSequenceJumps(digits, digits.size() / 2);
+  }
+
+  private int sumSequenceJumps(List<Integer> digits, int jump) {
+
+    int originalLength = digits.size();
+    int i = 0;
+    while (i < jump) {
+      digits.add(digits.get(i++));
     }
+    int sum = 0;
+    int index = 0;
+    while (index <= originalLength - 1) {
+      int current = digits.get(index);
+      int compareTo = digits.get(index + jump);
 
-    public int sumHalf(String captcha) {
-        List<Integer> digits = captcha.chars().map(i -> i - 48).boxed().collect(Collectors.toList());
-        return sumSequenceJumps(digits, digits.size() / 2);
+      if (current == compareTo) {
+        sum += current;
+      }
+      index++;
     }
-
-    private int sumSequenceJumps(List<Integer> digits, int jump) {
-
-        int originalLength = digits.size();
-        int i = 0;
-        while (i < jump) {
-            digits.add(digits.get(i++));
-        }
-        int sum = 0;
-        int index = 0;
-        while (index <= originalLength - 1) {
-            int current = digits.get(index);
-            int compareTo = digits.get(index + jump);
-
-            if (current == compareTo) {
-                sum += current;
-            }
-            index++;
-        }
-        return sum;
-    }
+    return sum;
+  }
 }
