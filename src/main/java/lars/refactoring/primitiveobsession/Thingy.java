093e108822b8e00d16aa7c9d1b2064910ca6247c
Lars Eckart <lars.eckart@gmail.com>
2020-01-03 23:13:19 +0200

    refactor primitive obsession
    as seen at http://blog.orfjackal.net/2013/03/refactoring-primitive-obsession.html

---
+++ src/main/java/lars/refactoring/primitiveobsession/Thingy.java
@@ -0,0 +1,45 @@
+package lars.refactoring.primitiveobsession;
+
+/**
+ * 1) Starting Small, change from int to ThingyId inside of Thingy only and make it compile again.
+ * 2) Pushing Arguments Out, extract parameter in setter
+ * 3) Pushing Return Values, Extract Method on field in getter and then Inline original getter and rename extracted method.
+ * 4) Pushing Arguments In
+ */
+public class Thingy {
+
+    private int thingyId;
+
+    public int getThingyId() {
+        return thingyId;
+    }
+
+    public void setThingyId(int thingyId) {
+        this.thingyId = thingyId;
+    }
+}
+
+class PseudoThingyClient {
+
+    public void assignThingy() {
+        Thingy thingy = new Thingy();
+        thingy.setThingyId(3);
+    }
+
+    public void useThingy() {
+        Thingy thingy = new Thingy();
+        int thingyId = thingy.getThingyId();
+        doThingyStuff(thingyId);
+    }
+
+    private void doThingyStuff(int thingyId) {
+        Gizmo g = new Gizmo();
+        g.doTheThing(thingyId); // here also rewrite to new ThingyId(thingyId).intValue() and extract parameter then
+    }
+}
+
+class Gizmo{
+
+    public void doTheThing(int thingyId) {
+    }
+}
---
+++ src/main/java/lars/refactoring/primitiveobsession/ThingyId.java
@@ -0,0 +1,33 @@
+package lars.refactoring.primitiveobsession;
+
+public final class ThingyId {
+
+    private final int id;
+
+    public ThingyId(int id) {
+        this.id = id;
+    }
+
+    public int toInt() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof ThingyId)) {
+            return false;
+        }
+        ThingyId that = (ThingyId) obj;
+        return this.id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "(" + id + ")";
+    }
+}
