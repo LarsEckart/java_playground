345f4f823b56311f114a71183abcd93ed2886c4c
Lars Eckart <lars.eckart@gmail.com>
2020-02-06 22:34:14 +0200

    didnt know that Arrays.asList and List.of actually behave differently here

--- src/test/java/lars/spielplatz/containers/EmptyTest.java
+++ src/test/java/lars/spielplatz/containers/EmptyTest.java
@@ -1,24 +1,36 @@
 package lars.spielplatz.containers;
 
-import org.junit.jupiter.api.Test;
-
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class EmptyTest {
 
+  @Test
+  void will_throw_when_checking_contains_null() {
+    assertThrows(NullPointerException.class, () -> List.<String>of().contains(null));
+  }
 
-    @Test
-    void will_throw_when_checking_contains_null() {
-        assertThrows(NullPointerException.class, () -> List.<String>of().contains(null));
-    }
+  @Test
+  void will_NOT_throw_when_checking_contains_null() {
+    assertThat(Collections.<String>emptyList().contains(null)).isFalse();
+  }
 
-    @Test
-    void will_NOT_throw_when_checking_contains_null() {
-        assertThat(Collections.<String>emptyList().contains(null)).isFalse();
-    }
+  @Test
+  void arrays_as_list_is_modifyable() {
+    List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6);
+    integers.set(0, 0);
+    assertThat(integers.get(0)).isEqualTo(0);
+  }
 
+  @Test
+  void list_of_is_not_modifyable() {
+    List<Integer> integers = List.of(1, 2, 3, 4, 5, 6);
+    assertThrows(UnsupportedOperationException.class, () -> integers.set(0, 0));
+  }
 }
