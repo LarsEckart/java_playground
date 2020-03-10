a9bec2bcb8930d2fe92c2d5a14f075a8fa05d8e8
Lars Eckart <lars.eckart@gmail.com>
2020-03-10 08:18:49 +0200

    remove undoable kata

--- src/main/java/lars/katas/spellcheck/SpellChecker.java
+++
@@ -1,8 +0,0 @@
-package lars.katas.spellcheck;
-
-interface SpellChecker {
-
-    public void addWord(String word);
-
-    public boolean checkCorrectness(String word, MultiMap<Integer, String> nearMatches);
-}
