0db3ff254d722ebee5f7e9012b4fe6d114591c26
Lars Eckart <lars.eckart@gmail.com>
2020-05-23 12:39:25 +0300

    formatting

--- src/test/java/lars/katas/stackcalculator/CalculatorTest.java
+++ src/test/java/lars/katas/stackcalculator/CalculatorTest.java
@@ -1,16 +1,15 @@
 package lars.katas.stackcalculator;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigInteger;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 
   @Test
   void first_example() {
-      Calculator calculator = new Calculator();
+    Calculator calculator = new Calculator();
 
     calculator.press("3");
     calculator.push();
@@ -20,21 +19,23 @@ class CalculatorTest {
     calculator.add();
     calculator.multiply();
 
-    assertThat(calculator.pop()).isEqualTo(new BigInteger("27"));;
+    assertThat(calculator.pop()).isEqualTo(new BigInteger("27"));
+    ;
   }
 
   @Test
   void second_example() {
-      Calculator calculator = new Calculator();
+    Calculator calculator = new Calculator();
 
     calculator.press("3");
     calculator.push();
     calculator.press("7");
-      calculator.multiply();
-      calculator.push();
-      calculator.press("2");
-      calculator.add();
+    calculator.multiply();
+    calculator.push();
+    calculator.press("2");
+    calculator.add();
 
-    assertThat(calculator.pop()).isEqualTo(new BigInteger("23"));;
+    assertThat(calculator.pop()).isEqualTo(new BigInteger("23"));
+    ;
   }
 }
