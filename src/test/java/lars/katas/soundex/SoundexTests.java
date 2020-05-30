ec4ff401086ad84ae3a719155980da76611c9ef9
Lars Eckart <lars.eckart@gmail.com>
2020-05-30 13:58:34 +0300

    tdd soundex

--- src/test/java/lars/katas/soundex/SoundexTests.java
+++ src/test/java/lars/katas/soundex/SoundexTests.java
@@ -1,15 +1,33 @@
 package lars.katas.soundex;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 
 class SoundexTests {
 
   @Test
-  void SoundexEncodingRetainsSoleLetterOfOneLetterWord() {
-    Soundex soundex;
+  void soundexEncodingRetainsSoleLetterOfOneLetterWord() {
+    Soundex soundex = new Soundex();
+    String encoded = soundex.encode("A");
+    assertThat(encoded).isEqualTo("A000");
+  }
+
+  @Test
+  void soundexEncodingPadsWithZerosToEnsureThreeDigits() {
+    Soundex soundex = new Soundex();
+    String encoded = soundex.encode("I");
+    assertThat(encoded).isEqualTo("I000");
   }
 
   private static class Soundex {
 
+    public String encode(String word) {
+      return zeroPad(word);
+    }
+
+    private String zeroPad(String word) {
+      return word + "000";
+    }
   }
 }
