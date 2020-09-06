4a09d6e313248e9045440de123a13a6cd8c05522
Lars Eckart <lars.eckart@gmail.com>
2020-09-06 21:10:45 +0300

    another way on how to skip invalid values in a stream

--- src/main/java/lars/spielplatz/CsvFileAnalysis.java
+++ src/main/java/lars/spielplatz/CsvFileAnalysis.java
@@ -3,6 +3,7 @@ package lars.spielplatz;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 class CsvFileAnalysis {
@@ -15,6 +16,17 @@ class CsvFileAnalysis {
       lines
           .filter(l -> !l.startsWith("#"))
           .flatMap(CsvFileAnalysis::lineToPerson)
+          .forEach(System.out::println);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    try (Stream<String> lines = Files.lines(p)) {
+      lines
+          .filter(l -> !l.startsWith("#"))
+          .map(CsvFileAnalysis::lineToPerson2)
+          .filter(Optional::isPresent)
+          .map(Optional::get)
           .forEach(System.out::println);
     } catch (IOException e) {
       e.printStackTrace();
@@ -32,6 +44,19 @@ class CsvFileAnalysis {
     } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
       return Stream.empty();
     }
+  }
+
+  private static Optional<Person> lineToPerson2(String line) {
+      try {
+        String[] elements = line.split(",");
+        String name = elements[0];
+        int age = Integer.parseInt(elements[1]);
+        String city = elements[2];
+        Person p = new Person(name, age, city);
+        return Optional.of(p);
+      } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+        return Optional.empty();
+      }
   }
 
   record Person(String name, int age, String city) {}
