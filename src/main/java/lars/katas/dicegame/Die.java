675e2178a1648b7d96435d2f595dab2a07a06bc1
Lars Eckart <lars.eckart@gmail.com>
2020-09-18 22:31:53 +0300

    F implement a dice

--- src/main/java/lars/katas/dicegame/Die.java
+++ src/main/java/lars/katas/dicegame/Die.java
@@ -3,4 +3,8 @@ package lars.katas.dicegame;class Die {
   public int faceValue() {
     return 1;
   }
+
+  public void roll() {
+
+  }
 }
--- src/test/java/lars/katas/dicegame/Die_should.java
+++ src/test/java/lars/katas/dicegame/Die_should.java
@@ -1,6 +1,7 @@
 package lars.katas.dicegame;
 
 import com.github.larseckart.tcr.TestCommitRevertMainExtension;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -12,6 +13,17 @@ class Die_should {
   @Test
   void initially_be_between_1_and_6() throws Exception {
     Die die = new Die();
+    verifyDieValue(die);
+  }
+
+  @RepeatedTest(1000)
+  void roll_between_1_and_6() throws Exception {
+    Die die = new Die();
+    die.roll();
+    verifyDieValue(die);
+  }
+
+  private void verifyDieValue(Die die) {
     assertThat(die.faceValue()).isGreaterThanOrEqualTo(1);
     assertThat(die.faceValue()).isLessThanOrEqualTo(6);
   }
