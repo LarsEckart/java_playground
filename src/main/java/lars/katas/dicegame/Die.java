8ef0e96ff93a0cc0ced0521527a3e6ac6fab20f4
Lars Eckart <lars.eckart@gmail.com>
2020-09-18 22:42:34 +0300

    F implement a dice

--- src/main/java/lars/katas/dicegame/Die.java
+++ src/main/java/lars/katas/dicegame/Die.java
@@ -1,10 +1,22 @@
-package lars.katas.dicegame;class Die {
+package lars.katas.dicegame;
+
+import java.util.Random;
+
+class Die {
+
+  private int value;
+  private final Random r;
+
+  public Die() {
+    value = 1;
+    r = new Random();
+  }
 
   public int faceValue() {
-    return 1;
+    return value;
   }
 
   public void roll() {
-
+    value = r.nextInt(6) + 1;
   }
 }
--- src/test/java/lars/katas/dicegame/Die_should.java
+++ src/test/java/lars/katas/dicegame/Die_should.java
@@ -1,6 +1,7 @@
 package lars.katas.dicegame;
 
 import com.github.larseckart.tcr.TestCommitRevertMainExtension;
+import java.util.Arrays;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,6 +15,21 @@ class Die_should {
   void initially_be_between_1_and_6() throws Exception {
     Die die = new Die();
     verifyDieValue(die);
+  }
+
+  @Test
+  void distributed_well() throws Exception {
+    Die die = new Die();
+    int[] outcome = new int[6];
+    Arrays.fill(outcome, 0);
+    for (int i = 0; i < 600000; i++) {
+      die.roll();
+      outcome[die.faceValue() - 1] = outcome[die.faceValue() - 1] + 1;
+    }
+    System.out.println(Arrays.toString(outcome));
+    for (int j : outcome) {
+      assertThat(j).isBetween(95000, 105000);
+    }
   }
 
   @RepeatedTest(1000)
