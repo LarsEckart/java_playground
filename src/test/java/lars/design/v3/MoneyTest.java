c52ba7e526d7611f0f6e79e391ee783c20e7319e
Lars Eckart <lars.eckart@gmail.com>
2020-10-25 21:24:03 +0200

    currency converter, example test when mocking our own abstraction

---
+++ src/test/java/lars/design/v3/MoneyTest.java
@@ -0,0 +1,22 @@
+package lars.design.v3;
+
+import java.math.BigDecimal;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
+class MoneyTest {
+
+  @Test
+  void test_currency_conversion() {
+    var converter = new CurrencyConverter((from, to) -> new ExchangeRate(from, to, 2.0));
+
+    Money converted =
+        converter.convert(new Money(Currency.of("EUR"), BigDecimal.TEN), Currency.of("AUD"));
+
+    assertThat(converted).isEqualTo(new Money(Currency.of("AUD"), new BigDecimal("20")));
+  }
+}
