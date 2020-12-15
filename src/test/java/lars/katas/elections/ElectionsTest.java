81363c6214b75be5e2e31a0cc9f71e63e2c713d0
Lars Eckart <lars.eckart@gmail.com>
2020-12-15 19:13:10 +0200

    elections kata
    shown here: https://youtu.be/3ezxq5XU5ek

---
+++ src/test/java/lars/katas/elections/ElectionsTest.java
@@ -0,0 +1,77 @@
+package lars.katas.elections;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ElectionsTest {
+
+  @Test
+  void electionWithoutDistricts() {
+    Map<String, List<String>> list = Map.of(
+        "District 1", Arrays.asList("Bob", "Anna", "Jess", "July"),
+        "District 2", Arrays.asList("Jerry", "Simon"),
+        "District 3", Arrays.asList("Johnny", "Matt", "Carole")
+    );
+    Elections elections = new Elections(list, false);
+    elections.addCandidate("Michel");
+    elections.addCandidate("Jerry");
+    elections.addCandidate("Johnny");
+
+    elections.voteFor("Bob", "Jerry", "District 1");
+    elections.voteFor("Jerry", "Jerry", "District 2");
+    elections.voteFor("Anna", "Johnny", "District 1");
+    elections.voteFor("Johnny", "Johnny", "District 3");
+    elections.voteFor("Matt", "Donald", "District 3");
+    elections.voteFor("Jess", "Joe", "District 1");
+    elections.voteFor("Simon", "", "District 2");
+    elections.voteFor("Carole", "", "District 3");
+
+    Map<String, String> results = elections.results();
+
+    Map<String, String> expectedResults = Map.of(
+        "Jerry", "50,00%",
+        "Johnny", "50,00%",
+        "Michel", "0,00%",
+        "Blank", "25,00%",
+        "Null", "25,00%",
+        "Abstention", "11,11%");
+    Assertions.assertThat(results).isEqualTo(expectedResults);
+  }
+
+  @Test
+  void electionWithDistricts() {
+    Map<String, List<String>> list = Map.of(
+        "District 1", Arrays.asList("Bob", "Anna", "Jess", "July"),
+        "District 2", Arrays.asList("Jerry", "Simon"),
+        "District 3", Arrays.asList("Johnny", "Matt", "Carole")
+    );
+    Elections elections = new Elections(list, true);
+    elections.addCandidate("Michel");
+    elections.addCandidate("Jerry");
+    elections.addCandidate("Johnny");
+
+    elections.voteFor("Bob", "Jerry", "District 1");
+    elections.voteFor("Jerry", "Jerry", "District 2");
+    elections.voteFor("Anna", "Johnny", "District 1");
+    elections.voteFor("Johnny", "Johnny", "District 3");
+    elections.voteFor("Matt", "Donald", "District 3");
+    elections.voteFor("Jess", "Joe", "District 1");
+    elections.voteFor("July", "Jerry", "District 1");
+    elections.voteFor("Simon", "", "District 2");
+    elections.voteFor("Carole", "", "District 3");
+
+    Map<String, String> results = elections.results();
+
+    Map<String, String> expectedResults = Map.of(
+        "Jerry", "66,67%",
+        "Johnny", "33,33%",
+        "Michel", "0,00%",
+        "Blank", "22,22%",
+        "Null", "22,22%",
+        "Abstention", "0,00%");
+    Assertions.assertThat(results).isEqualTo(expectedResults);
+  }
+}
