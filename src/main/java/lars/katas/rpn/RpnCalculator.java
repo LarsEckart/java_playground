54797ff727aa15c1b7328e286d603bb3250bdb9d
Lars Eckart <lars.eckart@hey.com>
2021-02-23 17:56:17 +0200

    we can now add and remove values from the calculator

--- src/main/java/lars/katas/rpn/RpnCalculator.java
+++ src/main/java/lars/katas/rpn/RpnCalculator.java
@@ -1,16 +1,31 @@
 package lars.katas.rpn;
 
 import java.math.BigDecimal;
+import java.util.Stack;
 
 public class RpnCalculator {
 
-  private BigDecimal accumulator = BigDecimal.ZERO;
+  private Stack<BigDecimal> values = new Stack<>();
 
   public BigDecimal getAccumulator() {
-    return accumulator;
+    if (values.size() > 0) {
+      return values.peek();
+    }
+    return BigDecimal.ZERO;
   }
 
   public void setAccumulator(BigDecimal value) {
-    this.accumulator = value;
+    if (values.size() > 0) {
+      values.pop();
+    }
+    values.push(value);
+  }
+
+  public void enter() {
+    values.push(getAccumulator());
+  }
+
+  public void drop() {
+    values.pop();
   }
 }
--- src/test/java/lars/katas/rpn/Newly_Created_Calculator_should.java
+++ src/test/java/lars/katas/rpn/Newly_Created_Calculator_should.java
@@ -2,13 +2,13 @@ package lars.katas.rpn;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.github.larseckart.tcr.TestCommitRevertMainExtension;
+import com.github.larseckart.tcr.CommitOnGreenExtension;
 import java.math.BigDecimal;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-@ExtendWith(TestCommitRevertMainExtension.class)
+@ExtendWith(CommitOnGreenExtension.class)
 public class Newly_Created_Calculator_should {
 
   private RpnCalculator calculator;
@@ -29,5 +29,24 @@ public class Newly_Created_Calculator_should {
     RpnCalculator calculator = new RpnCalculator();
     calculator.setAccumulator(value);
     assertThat(calculator.getAccumulator()).isEqualTo(value);
+  }
+
+  @Test
+  void allow_multiple_values_to_be_stored() {
+    BigDecimal value = new BigDecimal(42);
+    BigDecimal value2 = new BigDecimal(2);
+    BigDecimal value3 = new BigDecimal(3);
+    calculator.setAccumulator(value);
+    calculator.enter();
+    calculator.setAccumulator(value2);
+    calculator.enter();
+    calculator.setAccumulator(value3);
+    assertThat(calculator.getAccumulator()).isEqualTo(value3);
+    calculator.drop();
+    assertThat(calculator.getAccumulator()).isEqualTo(value2);
+    calculator.drop();
+    assertThat(calculator.getAccumulator()).isEqualTo(value);
+    calculator.drop();
+    assertThat(calculator.getAccumulator()).isEqualTo(BigDecimal.ZERO);
   }
 }
