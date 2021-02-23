4dee8b4d30bbc4e0f14153e82641875a071f196b
Lars Eckart <lars.eckart@hey.com>
2021-02-23 16:32:01 +0200

    r rename test class and methods

--- src/test/java/lars/katas/rpn/Newly_Created_Calculator_should.java
+++ src/test/java/lars/katas/rpn/Newly_Created_Calculator_should.java
@@ -4,20 +4,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.github.larseckart.tcr.TestCommitRevertMainExtension;
 import java.math.BigDecimal;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(TestCommitRevertMainExtension.class)
-public class RpnCalculatorTest {
+public class Newly_Created_Calculator_should {
+
+  private RpnCalculator calculator;
+
+  @BeforeEach
+  void setUp() {
+    calculator = new RpnCalculator();
+  }
 
   @Test
-  void new_calculator_has_a_zero_in_its_accumulator() {
-    RpnCalculator calculator = new RpnCalculator();
+  void have_zero_in_its_accumulator() {
     assertThat(calculator.getAccumulator()).isEqualTo(BigDecimal.ZERO);
   }
 
   @Test
-  void new_calculator_should_allow_its_accumulator_to_be_set() {
+  void allow_its_accumulator_to_be_set() {
     BigDecimal value = new BigDecimal(42);
     RpnCalculator calculator = new RpnCalculator();
     calculator.setAccumulator(value);
