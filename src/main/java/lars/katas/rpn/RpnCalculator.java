385caf8b85ca6fd9ab99523acb439cf72de148c6
Lars Eckart <lars.eckart@hey.com>
2021-02-23 16:07:01 +0200

    F we can not set the value of the calculator

--- src/main/java/lars/katas/rpn/RpnCalculator.java
+++ src/main/java/lars/katas/rpn/RpnCalculator.java
@@ -3,7 +3,13 @@ package lars.katas.rpn;
 import java.math.BigDecimal;
 
 public class RpnCalculator {
+    private BigDecimal accumulator = new BigDecimal(0);
+
     public BigDecimal getAccumulator() {
-        return BigDecimal.ZERO;
+        return accumulator;
+    }
+
+    public void setAccumulator(BigDecimal value) {
+        this.accumulator = value;
     }
 }
--- src/test/java/lars/katas/rpn/RpnCalculatorTest.java
+++ src/test/java/lars/katas/rpn/RpnCalculatorTest.java
@@ -4,8 +4,10 @@ import com.github.larseckart.tcr.TestCommitRevertMainExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import java.lang.invoke.VarHandle;
 import java.math.BigDecimal;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(TestCommitRevertMainExtension.class)
@@ -15,5 +17,13 @@ public class RpnCalculatorTest {
     void new_calculator_has_a_zero_in_its_accumulator() {
         RpnCalculator calculator = new RpnCalculator();
         assertThat(calculator.getAccumulator()).isEqualTo(BigDecimal.ZERO);
+    }
+
+    @Test
+    void new_calculator_should_allow_its_accumulator_to_be_set() {
+        BigDecimal value = new BigDecimal(42);
+        RpnCalculator calculator = new RpnCalculator();
+        calculator.setAccumulator(value);
+        assertThat(calculator.getAccumulator()).isEqualTo(value);
     }
 }
