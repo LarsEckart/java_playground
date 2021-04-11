4264e296111ae48bd776484a4eecd274d03342ec
Lars Eckart <lars.eckart@hey.com>
2021-04-11 19:02:31 +0300

    use jana.nio for finding gradle projects

--- src/main/java/lars/scripts/FindGradleRepoAndReportVersion.java
+++ src/main/java/lars/scripts/FindGradleRepoAndReportVersion.java
@@ -1,67 +1,55 @@
 package lars.scripts;
 
-import java.io.File;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FindGradleRepoAndReportVersion {
 
-  public static void main(String[] args) throws IOException, InterruptedException {
-    final List<File> gradleProjects =
-        Arrays.stream(new File("/Users/lars/GitHub/").listFiles(File::isDirectory))
-            .filter(f -> Files.exists(Path.of(f.getAbsoluteFile() + File.separator + "gradle")))
-            .sorted()
-            .toList();
+  private static final Logger log = getLogger(FindGradleRepoAndReportVersion.class);
 
-    for (int i = 0, gradleProjectsSize = gradleProjects.size(); i < gradleProjectsSize; i++) {
-      File gradleProject = gradleProjects.get(i);
-      // todo, check if android app
-      final List<String> androidApps =
+  public static void main(String[] args) throws IOException, InterruptedException {
+    final List<Path> gradleProjects = findAllGradleProjects();
+
+    for (Path gradleProject : gradleProjects) {
+      final List<String> complicatedOrAndroidGradleProject =
           List.of(
-              "/Users/lars/GitHub/WeatherStation",
-              "/Users/lars/GitHub/cveeviewer",
-              "/Users/lars/GitHub/android-boilerplate",
-              "/Users/lars/GitHub/cv_ee_viwer_2",
               "/Users/lars/GitHub/fairy-fingers",
               "/Users/lars/GitHub/fortune-cookie-shop",
+              "/Users/lars/GitHub/qualitymatters",
               "/Users/lars/GitHub/componentizationArch");
-      if (androidApps.contains(gradleProject.getAbsolutePath())) {
+      if (complicatedOrAndroidGradleProject.contains(gradleProject.toString())) {
+        log.info("android app got through: " + gradleProject);
         continue;
       }
-      final Path path =
-          Path.of(gradleProject.getAbsolutePath() + "/gradle/wrapper/gradle-wrapper.properties");
-      final String version =
-          Files.lines(path)
-              .filter(l -> l.startsWith("distributionUrl"))
-              .map(l -> l.substring(66))
-              .map(s -> s.replace("-all.zip", ""))
-              .map(s -> s.replace("-bin.zip", ""))
-              .findFirst()
-              .orElse("boom");
+      final String version = determineGradleWrapperVersion(gradleProject);
       System.out.println(gradleProject + " : " + version);
 
       // TODO: but what if behind origin/master?
-      if ("7.0".equals(version)) {
-        continue;
-      }
+
 
       try {
         Process updateGradleWrapperProcess =
-            Runtime.getRuntime().exec("gradle wrapper", null, gradleProject);
+            Runtime.getRuntime().exec("gradle wrapper", null, gradleProject.toFile());
         updateGradleWrapperProcess.waitFor();
         if (updateGradleWrapperProcess.exitValue() != 0) {
           System.err.println("%s need manual tweaking".formatted(gradleProject));
-          break;
+          continue;
         }
         Process verifyGradleProjectBuilds =
-            Runtime.getRuntime().exec("./gradlew test", null, gradleProject);
+            Runtime.getRuntime().exec("./gradlew test", null, gradleProject.toFile());
         verifyGradleProjectBuilds.waitFor();
         if (verifyGradleProjectBuilds.exitValue() != 0) {
           System.err.println("Gradle build in %s needs manual tweaking".formatted(gradleProject));
-          break;
+          continue;
         }
       } catch (Exception e) {
         System.out.println("something went wrong");
@@ -71,13 +59,46 @@ public class FindGradleRepoAndReportVersion {
               .exec(
                   new String[] {"git", "commit", "-a", "-m", "'update gradle wrapper'"},
                   null,
-                  gradleProject);
+                  gradleProject.toFile());
       gitCommitCommand.waitFor();
 
       System.out.println("done at %s".formatted(gradleProject));
-      if (i == 5) {
-        break;
-      }
     }
+  }
+
+  @NotNull
+  private static String determineGradleWrapperVersion(Path gradleProject) throws IOException {
+    final Path path = gradleProject.resolve("gradle/wrapper/gradle-wrapper.properties");
+    final String version =
+        Files.lines(path)
+            .filter(l -> l.startsWith("distributionUrl"))
+            .map(l -> l.substring(66))
+            .map(s -> s.replace("-all.zip", ""))
+            .map(s -> s.replace("-bin.zip", ""))
+            .findFirst()
+            .orElse("boom");
+    return version;
+  }
+
+  private static List<Path> findAllGradleProjects() {
+    final List<Path> gradleProjects = new ArrayList<>();
+    DirectoryStream.Filter<Path> filter =
+        file -> {
+          final boolean isDirectory = Files.isDirectory(file);
+          boolean hasGradleSubFolder = Files.exists(file.resolve(Path.of("gradle")));
+          boolean hasGradleBuildFile = Files.exists(file.resolve(Path.of("build.gradle")));
+          boolean isProbablyAndroidApp = Files.exists(file.resolve(Path.of("app")));
+          return isDirectory && hasGradleSubFolder && hasGradleBuildFile && !isProbablyAndroidApp;
+        };
+
+    try (var stream = Files.newDirectoryStream(Path.of("/Users/lars/GitHub/"), filter)) {
+      for (Path path : stream) {
+        gradleProjects.add(path);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return gradleProjects.stream().sorted().toList();
   }
 }
