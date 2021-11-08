1251aa4ca09fd1b39e96ddecb14c5ebe2674d64a
Lars Eckart <lars.eckart@hey.com>
2021-11-08 14:05:46 +0200

    MegaMart from grokking simplicity

---
+++ src/main/java/lars/functional/MegaMart.java
@@ -0,0 +1,34 @@
+package lars.functional;
+
+import java.util.List;
+
+public class MegaMart {
+
+  private List<Item> shoppingCart;
+  private int shoppingCartTotal = 0;
+
+  public void addItemToCart(String name, int price) {
+    shoppingCart.add(new Item(name, price));
+    calculateCartTotal();
+  }
+
+  private void calculateCartTotal() {
+    shoppingCartTotal = 0;
+    for (int i = 0; i < shoppingCart.size(); i++) {
+      shoppingCartTotal = +shoppingCart.get(i).price();
+    }
+    setCartTotalDom();
+  }
+
+  private void updateShippingIcons() {
+    
+  }
+
+
+  private void setCartTotalDom() {
+
+  }
+
+  private static record Item(String name, int price) {}
+}
+
