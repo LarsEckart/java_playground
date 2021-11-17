dedcfaef0489cced51ba5977f0f1829bb52c9190
Lars Eckart <lars.eckart@hey.com>
2021-11-17 22:21:18 +0200

    expense report kata

---
+++ src/test/java/lars/katas/expensereport/ExpenseReportTest.java
@@ -0,0 +1,16 @@
+package lars.katas.expensereport;
+
+import com.github.larseckart.tcr.FastTestCommitRevertMainExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(FastTestCommitRevertMainExtension.class)
+class ExpenseReportTest {
+
+  @Test
+  void approval() {
+
+  }
+}
+
+
