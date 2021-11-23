547662bce510b2cc14d6e0f51f90ce4d53644533
Lars Eckart <lars.eckart@hey.com>
2021-11-23 09:45:03 +0200

    combination approvals

--- src/test/java/lars/katas/expensereport/ExpenseReportTest.java
+++ src/test/java/lars/katas/expensereport/ExpenseReportTest.java
@@ -7,6 +7,7 @@ import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.approvaltests.core.Options;
 import org.approvaltests.scrubbers.DateScrubber;
 import org.junit.jupiter.api.Test;
@@ -17,16 +18,19 @@ class ExpenseReportTest {
 
   @Test
   void approval() {
-    Approvals.verify(
-        createReport(), new Options(DateScrubber.getScrubberFor("Wed Nov 17 22:28:33 EET 2021")));
+    ExpenseType[] expenseType = new ExpenseType[]{ExpenseType.BREAKFAST, ExpenseType.DINNER, ExpenseType.CAR_RENTAL};
+    Integer[] amount = new Integer[]{0, 1, 999, 1000, 1001, 4999, 5000, 5001};
+
+    CombinationApprovals.verifyAllCombinations(
+        this::createReport, expenseType, amount, new Options(DateScrubber.getScrubberFor("Wed Nov 17 22:28:33 EET 2021")));
   }
 
-  private String createReport() {
+  private String createReport(ExpenseType expenseType, int amount) {
     final PrintStream systemOut = System.out;
     try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
       System.setOut(new PrintStream(baos, true, StandardCharsets.UTF_8));
 
-      run(ExpenseType.BREAKFAST, 1);
+      run(expenseType, amount);
 
       return baos.toString();
     } catch (IOException e) {
@@ -36,11 +40,11 @@ class ExpenseReportTest {
     }
   }
 
-  private void run(ExpenseType breakfast, int amount) {
+  private void run(ExpenseType expenseType, int amount) {
     ExpenseReport expenseReport = new ExpenseReport();
 
     Expense expense = new Expense();
-    expense.type = breakfast;
+    expense.type = expenseType;
     expense.amount = amount;
 
     expenseReport.printReport(List.of(expense));
