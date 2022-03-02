6546e52c154c6b195b9ba9a2d804bca55d23fde6
LarsEckart <lars.eckart@hey.com>
2022-03-02 13:53:57 +0200

    obfuscationutil from linkedin training

---
+++ src/main/java/lars/spielplatz/ObfuscationUtil.java
@@ -0,0 +1,30 @@
+package lars.spielplatz;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+import org.apache.commons.codec.binary.Hex;
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class ObfuscationUtil {
+
+    private static final Logger log = getLogger(ObfuscationUtil.class);
+
+    /**
+     * takes a string. runs it through cryptographic secure hash.
+     * then uses apache commons to encode it to hex string.
+     */
+    public static String obfuscateData(String data) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedHash = digest.digest(data.getBytes(StandardCharsets.UTF_8));
+            return new String(Hex.encodeHex(encodedHash));
+        } catch (Exception e) {
+            log.error("error hashing data, outputting empty string instead");
+            return "";
+        }
+    }
+
+}
