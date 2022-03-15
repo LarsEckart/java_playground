833e23f15388057945d7e800141c61e6182c0415
LarsEckart <lars.eckart@hey.com>
2022-03-15 18:09:21 +0200

    web socket example

---
+++ src/main/java/lars/spielplatz/java11/FormDataPublisher.java
@@ -0,0 +1,23 @@
+package lars.spielplatz.java11;
+
+import java.net.URLEncoder;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+public class FormDataPublisher {
+
+  public static BodyPublisher ofFormData(Map<Object, Object> data) {
+    var builder = new StringBuilder();
+    for (Map.Entry<Object, Object> entry : data.entrySet()) {
+      if (builder.length() > 0) {
+        builder.append("&");
+      }
+      builder.append(URLEncoder.encode(entry.getKey().toString(), StandardCharsets.UTF_8));
+      builder.append("=");
+      builder.append(URLEncoder.encode(entry.getValue().toString(), StandardCharsets.UTF_8));
+    }
+    return BodyPublishers.ofString(builder.toString());
+  }
+}
