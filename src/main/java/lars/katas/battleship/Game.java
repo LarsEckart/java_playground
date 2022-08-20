baaccc5597c8d937773486a9907612c0f03128e6
Lars Eckart <lars.eckart@hey.com>
2022-08-20 19:11:04 +0300

    F!! create random ship locations for game

--- src/main/java/lars/katas/battleship/Game.java
+++ src/main/java/lars/katas/battleship/Game.java
@@ -1,15 +1,31 @@
 package lars.katas.battleship;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Game {
 
   public static void main(String[] args) {
     Fleet fleet = new Fleet();
-    fleet.add(new Ship(List.of(new Coordinate(1))));
-    fleet.add(new Ship(List.of(new Coordinate(5), new Coordinate(6))));
-    fleet.add(new Ship(List.of(new Coordinate(9), new Coordinate(10), new Coordinate(11))));
+    Random random = new Random();
+    int i = random.nextInt(17);
+    Set<Coordinate> shipLocations = new HashSet<>();
+    Coordinate e1 = new Coordinate(i);
+    Coordinate e2 = new Coordinate(i + 1);
+    Coordinate e3 = new Coordinate(i + 2);
+    shipLocations.add(e1);
+    shipLocations.add(e2);
+    shipLocations.add(e3);
+    fleet.add(new Ship(List.of(e1, e2, e3)));
+
+    int nextInt = random.nextInt(17);
+    while (shipLocations.contains(new Coordinate(nextInt))) {
+      nextInt = random.nextInt(17);
+    }
+    fleet.add(new Ship(List.of(new Coordinate(nextInt))));
 
     while (fleet.anyAlive()) {
       int nextGuess = getUserInput("next guess?");
