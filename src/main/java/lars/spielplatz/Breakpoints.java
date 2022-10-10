fd195ca6fa11211f4c1be756fc0d31123157d944
Lars Eckart <lars.eckart@hey.com>
2022-10-10 11:16:30 +0300

    from https://twitter.com/GeoffreyDeSmet/status/1577262981427392514

---
+++ src/main/java/lars/spielplatz/Breakpoints.java
@@ -0,0 +1,20 @@
+package lars.spielplatz;
+
+public class Breakpoints {
+
+  public static void main(String[] args) {
+
+    calcAge(11);
+    calcAge(22);
+    calcAge(33); // set breakpoint but uncheck "Suspended"
+
+  }
+
+  static int calcAge(int any) {
+    System.out.println("calc");
+    System.out.println("calc");
+    System.out.println("calc"); // set breakpoint but check "disable until hitting following breakpoint"
+    return 42;
+  }
+
+}
