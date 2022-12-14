62825c5ff4cc49f79bc9ad6b66e953a0de9f09d6
Lars Eckart <lars.eckart@hey.com>
2022-12-14 11:29:16 +0200

    example of new equals implementation with java 17

--- src/main/java/lars/spielplatz/java16/Person.java
+++ src/main/java/lars/spielplatz/java16/Person.java
@@ -1,5 +1,7 @@
 package lars.spielplatz.java16;
 
+import java.util.Objects;
+
 record Person(String name, int age) {
   Person {
     if (age < 0) {
@@ -10,5 +12,16 @@ record Person(String name, int age) {
   private static int instanceCounter;
   static int getInstanceCounter() {
     return instanceCounter;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    return (o instanceof Person p)
+        && name.equalsIgnoreCase(p.name) && age == p.age;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, age);
   }
 }
