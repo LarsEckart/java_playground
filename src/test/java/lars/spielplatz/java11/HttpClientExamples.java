e437a6af9a7b7d7358a54298ee78dcd14b27a9c8
Lars Eckart <lars.eckart@hey.com>
2023-09-27 12:03:51 +0300

    be more flexible with expected user-agent header assertion

--- src/test/java/lars/spielplatz/java11/HttpClientExamples.java
+++ src/test/java/lars/spielplatz/java11/HttpClientExamples.java
@@ -138,7 +138,7 @@ public class HttpClientExamples {
     assertThat(recordedRequest.getHeader("Connection")).isEqualTo("Upgrade, HTTP2-Settings");
     assertThat(recordedRequest.getHeader("Content-Length")).isNull(); // was 0 previously!
     assertThat(recordedRequest.getHeader("Upgrade")).isEqualTo("h2c");
-    assertThat(recordedRequest.getHeader("User-Agent")).containsPattern("Java-http-client/1");
+    assertThat(recordedRequest.getHeader("User-Agent")).containsPattern("Java-http-client/(\\d+)");
     assertThat(recordedRequest.getHeader("Host")).isNotBlank();
   }
 
@@ -157,7 +157,7 @@ public class HttpClientExamples {
     assertThat(recordedRequest.getHeader("Connection")).isEqualTo("Upgrade, HTTP2-Settings");
     assertThat(recordedRequest.getHeader("Content-Length")).isEqualTo("0");
     assertThat(recordedRequest.getHeader("Upgrade")).isEqualTo("h2c");
-    assertThat(recordedRequest.getHeader("User-Agent")).containsPattern("Java-http-client/1");
+    assertThat(recordedRequest.getHeader("User-Agent")).containsPattern("Java-http-client/17");
     assertThat(recordedRequest.getHeader("Host")).isNotBlank();
   }
 
