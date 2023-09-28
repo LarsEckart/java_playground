3ca3dc5a52fee329e8925cce560d977b3e995401
Lars Eckart <lars.eckart@hey.com>
2023-09-28 16:49:13 +0300

    remove the test that was present to show issues with windows. pipeline should go green again

--- src/test/java/lars/spielplatz/MultiLineStringTest.java
+++ src/test/java/lars/spielplatz/MultiLineStringTest.java
@@ -1,15 +1,15 @@
 package lars.spielplatz;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.apache.commons.codec.digest.HmacAlgorithms;
-import org.apache.commons.codec.digest.HmacUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 class MultiLineStringTest {
 
+  @DisabledOnOs(OS.WINDOWS)
   @Test
   void line_endings_on_different_os() {
     String text = "%s%n%s%n%s%n%s".formatted("a", "b", "c", "d");
@@ -26,41 +26,6 @@ class MultiLineStringTest {
         () -> assertThat(text2).isEqualTo(text3),
         () -> assertThat(text).isEqualTo(text3),
         () -> assertThat(text).isEqualTo(text4),
-        () -> assertThat(text2).isEqualTo(text4),
-        () -> assertThat(text3).isEqualTo(text4)
-
-    );
-  }
-
-  @Test
-  void line_endings_on_different_os2() {
-
-      var text1 = new HmacUtils(HmacAlgorithms.HMAC_SHA_1, "anySecretKey".getBytes(UTF_8))
-          .hmacHex("%s%n%s%n%s%n%s".formatted("a", "b", "c", "d").getBytes(UTF_8))
-          .toUpperCase();
-
-      var text2 = new HmacUtils(HmacAlgorithms.HMAC_SHA_1, "anySecretKey".getBytes(UTF_8))
-          .hmacHex("%s\n%s\n%s\n%s".formatted("a", "b", "c", "d").getBytes(UTF_8))
-          .toUpperCase();
-
-      var text3 = new HmacUtils(HmacAlgorithms.HMAC_SHA_1, "anySecretKey".getBytes(UTF_8))
-          .hmacHex("""
-              %s
-              %s
-              %s
-              %s""".formatted("a", "b", "c", "d").getBytes(UTF_8))
-          .toUpperCase();
-    String text4 = new HmacUtils(HmacAlgorithms.HMAC_SHA_1, "anySecretKey".getBytes(UTF_8))
-        .hmacHex(String.join(System.lineSeparator(), "a", "b", "c", "d").getBytes(UTF_8))
-        .toUpperCase();
-
-    System.out.println("applesauce: " + text1);
-
-    Assertions.assertAll(
-        () -> assertThat(text1).isEqualTo(text2),
-        () -> assertThat(text2).isEqualTo(text3),
-        () -> assertThat(text1).isEqualTo(text3),
-        () -> assertThat(text1).isEqualTo(text4),
         () -> assertThat(text2).isEqualTo(text4),
         () -> assertThat(text3).isEqualTo(text4)
     );
