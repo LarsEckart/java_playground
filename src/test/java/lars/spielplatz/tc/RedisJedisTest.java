ba4cdaaca1f7dd9d133e6f4ba8438a96930c0b4e
Lars Eckart <lars.eckart@hey.com>
2023-11-10 12:54:12 +0200

    not using TCC anymore, thusenabling this test not for windows but only mac and linux

--- src/test/java/lars/spielplatz/tc/RedisJedisTest.java
+++ src/test/java/lars/spielplatz/tc/RedisJedisTest.java
@@ -1,12 +1,12 @@
 package lars.spielplatz.tc;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.slf4j.LoggerFactory.getLogger;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -14,10 +14,8 @@ import org.testcontainers.utility.DockerImageName;
 import redis.clients.jedis.Jedis;
 
 @Testcontainers
-// @EnabledOnOs({OS.LINUX, OS.MAC})
+@EnabledOnOs({OS.LINUX, OS.MAC})
 public class RedisJedisTest {
-
-  private static final Logger log = getLogger(RedisJedisTest.class);
 
   @Container
   private GenericContainer<?> redis =
@@ -33,7 +31,8 @@ public class RedisJedisTest {
   }
 
   @AfterEach
-  void tearDown() {}
+  void tearDown() {
+  }
 
   @Test
   void incr_for_integers() {
