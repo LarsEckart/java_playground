2bf1e2b75dc9f7030e65d47f2c63f44c84918f60
Lars Eckart <lars.eckart@hey.com>
2026-01-21 11:40:25 +0200

    fix: disable XXE processing in XML parsers
    Configure DocumentBuilderFactory with secure defaults:
    - Disallow DOCTYPE declarations
    - Disable external general/parameter entities
    - Disable external DTD loading
    - Disable XInclude and entity reference expansion
    
    Fixes 3 EarthQuakeParser instances (CWE-611)

--- src/main/java/lars/edu/duke/ClosestQuakesDemo/EarthQuakeParser.java
+++ src/main/java/lars/edu/duke/ClosestQuakesDemo/EarthQuakeParser.java
@@ -23,6 +23,14 @@ public class EarthQuakeParser {
     DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 
     try {
+      // Disable XXE (XML External Entity) processing
+      factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+      factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+      factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+      factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+      factory.setXIncludeAware(false);
+      factory.setExpandEntityReferences(false);
+
       DocumentBuilder builder = factory.newDocumentBuilder();
 
       // Document document = builder.parse(new File(source));
